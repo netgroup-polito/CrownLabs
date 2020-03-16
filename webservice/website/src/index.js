fe30f855659afb2e9e26fa68fe81b94aabd2c144
s41m0n <simonemagnani.96@gmail.com>
2020-03-16 19:30:52 +0100

    Fixed css inclusion in App.js file

--- webservice/website/src/index.js
+++ webservice/website/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const main = document.createElement('div');
 document.body.appendChild(main);
@@ -12,4 +12,4 @@ ReactDOM.render(<App />, main);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
