a39d86affb1f0639e03a4ca60cfb286c024f979f
Fulvio Risso <fulvio.risso@polito.it>
2020-03-18 15:57:12 +0100

    Update and rename convert_vm.sh to convert-vm.sh

--- vms/scripts/convert-vm.sh
+++ vms/scripts/convert-vm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $# -ne 1 ]; then
-    echo "Usage: convert_vm.sh <vdi_file>"
+    echo "Usage: convert-vm.sh <vdi_file>"
     exit 1
 fi
 
@@ -12,10 +12,10 @@ sudo apt-get install -y qemu-utils
 
 # Convert the image
 mkdir -p $DIR/docker_output
-qemu-img convert -f vdi -O qcow2 $1 docker_output/vm.qcow2
+qemu-img convert -f vdi -O qcow2 $1 docker-output/vm.qcow2
 
 # Create dockerfile for the build
-tee $DIR/docker_output/Dockerfile > /dev/null <<EOT
+tee $DIR/docker-output/Dockerfile > /dev/null <<EOT
 FROM scratch
 ADD vm.qcow2 /disk/
 EOT
