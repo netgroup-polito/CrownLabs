cff6794f9b21d888acbb2a2fb74cf85ace2a9d81
s41m0n <simonemagnani.96@gmail.com>
2020-03-19 01:11:06 +0100

    Added dynamic lab listing both for instances and templates

--- webservice/website/src/UserView.js
+++ webservice/website/src/UserView.js
@@ -9,63 +9,156 @@ import 'toastr/build/toastr.min.css'
 export default class UserView extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {labs: [], selectedCRD: "", runningCRD: ""};
+        this.connect = this.connect.bind(this);
+        this.changeSelectedCRD = this.changeSelectedCRD.bind(this);
+        this.startCRD = this.startCRD.bind(this);
+        this.stopCRD = this.stopCRD.bind(this);
+        this.notifyEvent = this.notifyEvent.bind(this);
+        this.state = {templateLabs: [], instanceLabs: new Map(), selectedCRD: null, events: ""};
         if (localStorage.getItem('token')) {
             this.apiManager = new ApiManager(localStorage.getItem('token'), localStorage.getItem('token_type'));
-            this.apiManager.getCRD()
+            this.apiManager.getCRDtemplate()
                 .then((nodesResponse) => {
                     const nodes = nodesResponse.body.items;
-                    this.setState({labs: nodes.map(x => {
-                        return x.metadata.name;
-                    })});
+                    this.setState({
+                        templateLabs: nodes.map(x => {
+                            return x.metadata.name;
+                        })
+                    });
+                    this.apiManager.startWatching(this.notifyEvent);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+            this.apiManager.getCRDinstance()
+                .then((nodesResponse) => {
+                    const nodes = nodesResponse.body.items;
+                    let toAdd = new Map(this.state.instanceLabs);
+                    for (const x in nodes) {
+                        toAdd.set(nodes[x].metadata.name, "http://example.com/");
+                    }
+                    this.setState({instanceLabs: toAdd});
                 })
                 .catch((error) => {
                     console.error(error);
                 });
         }
-        this.changeSelectedCRD = this.changeSelectedCRD.bind(this);
-        this.startCRD = this.startCRD.bind(this);
-        this.stopCRD = this.stopCRD.bind(this);
     }
+
+    notifyEvent(msg, obj) {
+        this.setState({events: this.state.events += msg += "\n"});
+    }
+
     changeSelectedCRD(name) {
         this.setState({selectedCRD: name});
     }
+
     startCRD() {
-        if(this.state.selectedCRD !== "") {
-            this.apiManager.createCRD(this.state.selectedCRD)
-                .then(
-                    (response) => {
-                        Toastr.success("Successfully started lab `" + this.state.selectedCRD + "`");
-                        this.setState({runningCRD: this.state.selectedCRD});
-                    },
-                    (err) => {
-                        console.log(err);
-                    }
-                );
+        if (!this.state.selectedCRD) {
+            Toastr.info("Please select a lab before starting it");
+            return;
         }
+        if (this.state.instanceLabs.has(this.state.selectedCRD)) {
+            Toastr.info("The `" + this.state.selectedCRD + '` lab is already running');
+            return;
+        }
+        this.apiManager.createCRD(this.state.selectedCRD)
+            .then(
+                (response) => {
+                    Toastr.success("Successfully started lab `" + this.state.selectedCRD + "`");
+                    const newMap = new Map(this.state.instanceLabs);
+                    newMap.set(this.state.selectedCRD, "http://example.com");
+                    this.setState({instanceLabs: newMap});
+                },
+                (error) => {
+                    let code = error.response._fetchResponse.status;
+                    let msg = "";
+                    switch (code) {
+                        case 409: {
+                            msg += "";
+                            break;
+                        }
+                        case 100 : {
+                            break;
+                        }
+                        default : {
+                            msg += "An unusual error occurred (" + code + "), please try again.";
+                        }
+                    }
+                    Toastr.error(msg);
+                }
+            )
+            .finally(() => {
+                this.changeSelectedCRD(null);
+            });
     }
+
     stopCRD() {
-        if(this.state.runningCRD !== "") {
-            this.apiManager.deleteCRD(this.state.runningCRD)
+        if (!this.state.selectedCRD) {
+            Toastr.info("No lab to stop has been selected");
+            return;
+        }
+        if (!this.state.instanceLabs.has(this.state.selectedCRD)) {
+            Toastr.info("The `" + this.state.selectedCRD + '` lab is not running');
+            return;
+        }
+        if (this.state.selectedCRD !== "") {
+            this.apiManager.deleteCRD(this.state.selectedCRD)
                 .then(
                     (response) => {
-                        Toastr.success("Successfully stopped `" + this.state.runningCRD + "`");
-                        this.setState({runningCRD: ""});
-                        console.log(response);
+                        Toastr.success("Successfully stopped `" + this.state.selectedCRD + "`");
+                        const newMap = new Map(this.state.instanceLabs);
+                        newMap.delete(this.state.selectedCRD);
+                        this.setState({instanceLabs: newMap});
                     },
                     (error) => {
-                        console.log(error);
+                        let code = error.response._fetchResponse.status;
+                        let msg = "";
+                        switch (code) {
+                            case 409: {
+                                msg += "";
+                                break;
+                            }
+                            case 100 : {
+                                break;
+                            }
+                            default : {
+                                msg += "An unusual error occurred (" + code + "), please try again.";
+                            }
+                        }
+                        Toastr.error(msg);
                     }
-                );
+                )
+                .finally(() => {
+                    this.changeSelectedCRD(null);
+                });
         }
     }
+
+    connect() {
+        if(!this.state.selectedCRD) {
+            Toastr.info("No lab selected to connect to");
+            return
+        }
+        if(!this.state.instanceLabs.has(this.state.selectedCRD)) {
+            Toastr.info("The lab `" + this.state.selectedCRD +  "` is not running");
+            return;
+        }
+        window.open(this.state.instanceLabs.get(this.state.selectedCRD), '_blank');
+        this.changeSelectedCRD(null);
+    }
+
     render() {
+        const keys = Array.from(this.state.instanceLabs.keys());
+        const runningLabs = keys.map(x => {
+            return <li key={x}><Button variant="link" onClick={() => this.changeSelectedCRD(x)}>{x}</Button></li>;
+        });
         return (
             <div style={{backgroundColor: '#F2F2F2'}}>
                 <Container className="cover">
                     <Row>
                         <Col className="col-3">
-                            <SideBar labs={this.state.labs} func={this.changeSelectedCRD}/>
+                            <SideBar labs={this.state.templateLabs} func={this.changeSelectedCRD}/>
                         </Col>
                         <Col className="col-9">
                             <Row className="my-5">
@@ -73,13 +166,16 @@ export default class UserView extends React.Component {
                                     <Card className="my-5 text-center headerstyle">
                                         <Card.Body>
                                             <Card.Text as="h6">Status information</Card.Text>
-                                            <textarea align="center" className="textareastyle"/>
+                                            <textarea readOnly align="center" className="textareastyle"
+                                                      value={this.state.events}/>
                                         </Card.Body>
                                         <Card.Footer className="headerstyle">
                                             <ButtonGroup aria-label="Basic example">
-                                                <Button variant="dark" className="text-success" onClick={this.startCRD}>Start</Button>
-                                                <Button variant="dark" className="text-danger" onClick={this.stopCRD}>Stop</Button>
-                                                <Button variant="dark">Connect</Button>
+                                                <Button variant="dark" className="text-success"
+                                                        onClick={this.startCRD}>Start</Button>
+                                                <Button variant="dark" className="text-danger"
+                                                        onClick={this.stopCRD}>Stop</Button>
+                                                <Button variant="dark" onClick={this.connect}>Connect</Button>
                                                 <Button variant="light"
                                                         onClick={this.props.authManager.logout}>Logout</Button>
                                             </ButtonGroup>
@@ -91,7 +187,9 @@ export default class UserView extends React.Component {
                                         <Card.Body>
                                             <Card.Text as="h6">Information</Card.Text>
                                             <p align="center">Selected lab: {this.state.selectedCRD}</p>
-                                            <p align="center">Running lab:  {this.state.runningCRD}</p>
+                                            <ul>
+                                                {runningLabs}
+                                            </ul>
                                         </Card.Body>
                                     </Card>
                                 </Col>
--- webservice/website/src/services/ApiManager.js
+++ webservice/website/src/services/ApiManager.js
@@ -1,4 +1,4 @@
-import {Config, CustomObjectsApi} from '@kubernetes/client-node';
+import {Config, CustomObjectsApi, watch } from '@kubernetes/client-node';
 
 export default class ApiManager {
     constructor(token, type) {
@@ -11,20 +11,23 @@ export default class ApiManager {
         this.instanceNamespace = 'test-simone';
         this.templatePlural = "labtemplates";
         this.instancePlural = "labinstances";
-        this.getCRD = this.getCRD.bind(this);
+        this.getCRDtemplate = this.getCRDtemplate.bind(this);
+        this.getCRDinstance = this.getCRDinstance.bind(this);
         this.createCRD = this.createCRD.bind(this);
         this.deleteCRD = this.deleteCRD.bind(this);
     }
-
-    getCRD() {
+    getCRDtemplate() {
         return this.api.listNamespacedCustomObject(this.templateGroup, this.version, this.templateNamespace, this.templatePlural);
+    }
+    getCRDinstance() {
+        return this.api.listNamespacedCustomObject(this.instanceGroup, this.version, this.instanceNamespace, this.instancePlural);
     }
     createCRD(labTemplateName) {
         return this.api.createNamespacedCustomObject(this.instanceGroup, this.version, this.instanceNamespace, this.instancePlural, {
             apiVersion: this.instanceGroup + "/" + this.version,
             kind: "LabInstance",
             metadata: {
-                name: labTemplateName + "-instance",
+                name: labTemplateName,
                 namespace: this.instanceNamespace,
             },
             spec: {
@@ -33,7 +36,28 @@ export default class ApiManager {
             }
         });
     }
-    deleteCRD(labTemplateName) {
-        return this.api.deleteNamespacedCustomObject(this.instanceGroup, this.version, this.instanceNamespace, this.instancePlural, labTemplateName + "-instance", {});
+    deleteCRD(name) {
+        return this.api.deleteNamespacedCustomObject(this.instanceGroup, this.version, this.instanceNamespace, this.instancePlural, name, {});
+    }
+    startWatching(func) {
+        /*
+        watch(this.kc, '/apis/instance.crown.team.com/v1/namespaces/test-simone/labinstances/watch/events', {},
+            function(type, object) {
+                let msg = "";
+                if (type === 'ADDED') {
+                    msg += 'Created Object';
+                } else if (type === 'MODIFIED') {
+                    msg += 'Modified Object';
+                } else if (type === 'DELETED') {
+                    msg += 'Deleted Object';
+                } else {
+                    msg += 'Unknown Event on object';
+                }
+                func(msg, object);
+            },
+            function(e) {
+                console.log('Stream ended', e);
+            }
+        );*/
     }
 }
