59613a212d7f7662709f6d699496ff8bec1eb6ff
Alex Palesandro <alex.palesandro@gmail.com>
2020-03-19 12:32:14 +0100

    WIP: Frontend/features (#43)
    This PR includes a working website compilable both locally or using the Dockerfile in the webservice directory.
    
    Please follow the README instruction to setup correctly your environment.
    
    Future improvements:
    
    * OIDC silent event intercept
    * Improve website (add more sections)
    * Conclude the API programming (retrieve pod status, in the next PR)
    
    Co-authored-by: s41m0n <simonemagnani.96@gmail.com>
    Co-authored-by: Serena <serena.flocco2@gmail.com>

--- webservice/website/src/App.js
+++ webservice/website/src/App.js
@@ -1,11 +1,63 @@
 import React from 'react';
-import Login from './Login';
+import Home from './Home';
+import UserView from './UserView';
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom';
 import './App.css';
+import CallBackHandler from "./CallBackHandler";
+import Authenticator from "./services/Authenticator";
 
-function App() {
-  return (
-    <Login />
-  );
+export class App extends React.Component {
+    constructor(props) {
+        super(props);
+        const authManager = new Authenticator();
+        if (localStorage.getItem('isLoggedIn')) {
+            this.state = {isLoggedIn: localStorage.getItem('isLoggedIn'), authManager: authManager};
+        } else {
+            this.state = {isLoggedIn: 'false', authManager: authManager};
+        }
+        authManager.manager.events.addUserLoaded(user => {
+            if (user != null) {
+                this.setState({isLoggedIn: 'true'}, () => {
+                    localStorage.setItem('isLoggedIn', 'true');
+                    localStorage.setItem('token', user.id_token);
+                    localStorage.setItem('token_type', user.token_type != null ? user.token_type : "Bearer");
+                });
+            } else {
+                localStorage.setItem('isLoggedIn', 'true');
+            }
+        });
+        authManager.manager.events.addUserUnloaded(() => {
+            localStorage.clear();
+            this.state = {isLoggedIn: 'false'};
+        })
+    }
+
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Home authManager={this.state.authManager}/>
+                    </Route>
+                    <Route path="/userview" render={() => (
+                        this.state.isLoggedIn === 'true' ?
+                            <UserView authManager={this.state.authManager}/> :
+                            <Redirect to="/"/>
+                    )}/>
+                    <Route path="/callback" render={() => (
+                        this.state.isLoggedIn === 'true' ? <Redirect to="/userview"/> :
+                            <CallBackHandler authManager={this.state.authManager} action={'login'}/>
+                    )}/>
+                    <Route path="/logout" render={() => (
+                        this.state.isLoggedIn === 'true' ?
+                            <CallBackHandler authManager={this.state.authManager} action={'logout'}/> :
+                            <Redirect to="/"/>
+                    )}/>
+                    <Route path="*">
+                        <Redirect to="/userview"/>
+                    </Route>
+                </Switch>
+            </Router>
+        );
+    }
 }
-
-export default App;
