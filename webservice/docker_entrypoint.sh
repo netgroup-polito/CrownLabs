5f3f2e0d291140941c18b6e6f7d02940d4fc7927
mLavacca <lavacca.mattia@gmail.com>
2020-03-19 15:03:27 +0100

    updates

--- webservice/docker_entrypoint.sh
+++ webservice/docker_entrypoint.sh
@@ -3,7 +3,7 @@
 echo "building the service..."
 npm run build
 
-mv /webservice/website/dist /usr/share/nginx/html
+mv ./webservice/website/dist /usr/share/nginx/html
 
 echo "starting nginx..."
 nginx -g 'daemon off;'
