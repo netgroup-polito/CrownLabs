7285cb1f4621e4b2df87a976a9d18379be399bf9
Serena Flocco <serena.flocco2@gmail.com>
2020-03-20 16:36:35 +0100

    Frontend Dockerfile - Add Configuration at runtime This commit enables the insertion of frontend configuration at Runtime. Parameters previously hardcoded in Dockerfile are dropped and the configuration is now passed in a configmap.

--- webservice/website/src/services/Authenticator.js
+++ webservice/website/src/services/Authenticator.js
@@ -4,19 +4,36 @@ import { UserManager } from 'oidc-client';
  * Api class to manage authN
  *
  */
+
 export default class Authenticator {
   constructor() {
+    if (window.OIDC_CLIENT_ID === undefined) {
+      window.OIDC_CLIENT_ID = OIDC_CLIENT_ID;
+    }
+
+    if (window.OIDC_PROVIDER_URL === undefined) {
+      window.OIDC_PROVIDER_URL = OIDC_PROVIDER_URL;
+    }
+
+    if (window.OIDC_CLIENT_SECRET === undefined) {
+      window.OIDC_CLIENT_SECRET = OIDC_CLIENT_SECRET;
+    }
+
+    if (window.OIDC_REDIRECT_URI === undefined) {
+      window.OIDC_REDIRECT_URI = OIDC_REDIRECT_URI;
+    }
+
     this.manager = new UserManager({
-      authority: OIDC_PROVIDER_URL,
-      client_id: OIDC_CLIENT_ID,
-      redirect_uri: OIDC_REDIRECT_URI + '/callback',
       automaticSilentRenew: true,
-      post_logout_redirect_uri: OIDC_REDIRECT_URI + '/',
       response_type: 'code',
       filterProtocolClaims: true,
       scope: 'openid ',
       loadUserInfo: true,
-      client_secret: OIDC_CLIENT_SECRET
+      client_secret: window.OIDC_CLIENT_SECRET,
+      authority: window.OIDC_PROVIDER_URL,
+      client_id: window.OIDC_CLIENT_ID,
+      redirect_uri: window.OIDC_REDIRECT_URI + '/callback',
+      post_logout_redirect_uri: window.OIDC_REDIRECT_URI + '/logout'
     });
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
---
+++ webservice/website/webpack_docker.config.js
@@ -0,0 +1,65 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const RobotstxtPlugin = require('robotstxt-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: __dirname,
+  entry: ['@babel/polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 8000,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      title: 'CrownLabs',
+      meta: {
+        viewport: 'width=device-width, initial-scale=1',
+        'theme-color': '#000000',
+        description: 'CrownLabs website'
+      }
+    }),
+    new RobotstxtPlugin({
+      'User-agent': '*',
+      Disallow: ''
+    })
+  ]
+};
