180dec084bb02e44b04181cfe83af000650924a2
s41m0n <simonemagnani.96@gmail.com>
2020-03-20 16:33:13 +0100

    Automatically discover namespaces and courses + Added watcher + Connect + refactored code

--- webservice/website/src/CallBackHandler.js
+++ webservice/website/src/CallBackHandler.js
@@ -1,9 +1,6 @@
 import React from "react";
 
 export default function CallBackHandler(props) {
-    if (props.action === 'login')
-        props.authManager.completeLogin();
-    else
-        props.authManager.completeLogout();
+    props.func();
     return (<div/>);
 }
