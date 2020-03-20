803ead1a933da833765f8dc48cae4d0fc880e2c4
s41m0n <simonemagnani.96@gmail.com>
2020-03-20 22:33:04 +0100

    Randomized instance name + fixed cluster error logout

--- webservice/website/src/components/Footer.js
+++ webservice/website/src/components/Footer.js
@@ -1,14 +1,16 @@
 import {Container} from "react-bootstrap";
 import React from "react";
 
-const githubIcon = require('../assets/github-logo.png');
-
+/**
+ * Function to draw the document footer
+ * @return the object to be drawn
+ */
 export default function funtionFooter() {
     return <footer className="py-4 blockquote-footer footerstyle">
         <Container fluid className="m-0 text-center text-secondary">
             <p className="d-inline">This software has been proudly developed at Politecnico di Torino. </p>
             <p className="d-inline">For info visit our</p>
-            <img className="d-inline" height="25px" src={githubIcon} alt="GitHub logo"/>
+            <img className="d-inline" height="25px" src={require('../assets/github-logo.png')} alt="GitHub logo"/>
             <a className="d-inline" href="https://github.com/netgroup-polito/CrownLabs">Github project
                 repository</a>
         </Container>
