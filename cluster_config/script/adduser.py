07694578ae5ab7375bc4ec57464da019b0158e82
Marco Iorio <marco.iorio@polito.it>
2020-03-21 10:52:32 +0100

    adduser.py - Increase robustness

--- cluster_config/script/adduser.py
+++ cluster_config/script/adduser.py
@@ -1,26 +1,37 @@
-import json
-import os
 import argparse
-import pandas as pd
+import os
+import sys
 import jinja2 as Template
-from keycloak import KeycloakAdmin, exceptions
+import keycloak as kc
+import pandas as pd
+
+
+class KeycloakHandlerException(Exception):
+    pass
+
 
 class KeycloakHandler:
     def __init__(self, admin_user, admin_pass, client_id):
-        self.keycloak_admin = KeycloakAdmin(
+        self.keycloak_admin = kc.KeycloakAdmin(
             server_url="https://auth.crown-labs.ipv6.polito.it/auth/",
             username=admin_user,
             password=admin_pass,
             user_realm_name="master",
             realm_name="crownlabs",
             verify=True)
+
         self.client_id = self.keycloak_admin.get_client_id(client_id)
+        if not self.client_id:
+            raise KeycloakHandlerException("Client ID '{}' not found in Keycloak".format(client_id))
+
+        self.client_roles = dict()
 
     def get_user(self, username):
-        return self.keycloak_admin.get_users({'username': username})
+        _users = self.keycloak_admin.get_users({'username': username})
+        return None if len(_users) == 0 else _users[0]
 
-    def create_new_user(self, email, username, first_name, last_name, course_code):
-        _user = self.keycloak_admin.create_user({
+    def create_new_user(self, email, username, first_name, last_name):
+        _user_data = {
             'email': email,
             'username': username,
             'firstName': first_name,
@@ -29,24 +40,40 @@ class KeycloakHandler:
             'emailVerified': True,
             'attributes': {
                 'namespace': "student-{}".format(username),
-                'courses': [course_code, ],
             }
-        })
-        self.keycloak_admin.send_update_account(user_id=_user, payload=json.dumps(['UPDATE_PASSWORD']))
+        }
 
-    def add_user_to_course(self, user_json, username, course_code):
-        _user_id = user_json.get('id')
-        _course_codes = list(set(user_json.get('attributes').get('courses') + [course_code, ]))
-        return self.keycloak_admin.update_user(user_id=_user_id, payload={'attributes': {'namespace': "student-{}".format(username), 'courses' : _course_codes}})
+        _user_id = self.keycloak_admin.create_user(_user_data)
+        self.keycloak_admin.send_update_account(user_id=_user_id, payload='["UPDATE_PASSWORD"]')
+
+        _user_data['id'] = _user_id
+        return _user_data
+
+    def add_user_to_course(self, user, course_code):
+        _user_id = user.get('id')
+        _attributes = user.get('attributes', {})
+        _courses = _attributes.get('courses', [])
+        _attributes['courses'] = list(set(_courses + [course_code, ]))
+
+        self.keycloak_admin.update_user(user_id=_user_id, payload={'attributes': _attributes})
+        self.keycloak_admin.assign_client_role(user_id=_user_id, client_id=self.client_id, roles=self.client_roles[course_code])
 
     def add_client_role(self, course_code):
+        # Do not try to add the client role if already stored locally
+        if course_code in self.client_roles.keys():
+            return
+
         try:
-            self.keycloak_admin.get_client_role(self.client_id, course_code)
-        except exceptions.KeycloakGetError:
-            self.keycloak_admin.create_client_role(client_role_id=self.client_id, payload={'name': course_code, 'clientRole': True})
-    
-    def add_user_role(self,_username,_course_code):
-        self.keycloak_admin.assign_client_role(user_id=self.keycloak_admin.get_user_id(_username), client_id=self.keycloak_admin.get_client_id("k8s"), roles=self.keycloak_admin.get_client_role(client_id=self.keycloak_admin.get_client_id("k8s"), role_name=_course_code))
+            # Retrieve the client role
+            self.client_roles[course_code] = self.keycloak_admin.get_client_role(self.client_id, course_code)
+        except kc.exceptions.KeycloakGetError:
+            # Create a new client role
+            self.keycloak_admin.create_client_role(
+                client_role_id=self.client_id,
+                payload={'name': course_code, 'clientRole': True})
+            # Retrieve the newly created client role
+            self.client_roles[course_code] = self.keycloak_admin.get_client_role(self.client_id, course_code)
+
 
 class KubernetesHandler:
     def __init__(self, template_namespace_path, template_rolebinging_file, template_regcred_file):
@@ -61,10 +88,11 @@ class KubernetesHandler:
         _privileges = self.template_rb.render(
             namespace_name="student-{}".format(username),
             course_code="kubernetes:{}".format(course_code), username=username)
+
         _secret = self.template_sc.render(namespace_name="student-{}".format(username))
-        os.system("echo '{}' | kubectl create -f -".format(_namespace))
-        os.system("echo '{}' | kubectl create -f -".format(_privileges))
-        os.system("echo '{}' | kubectl create -f -".format(_secret))
+        os.system("echo '{}' | kubectl apply -f - | sed 's/^/*** k8s: /g'".format(_namespace))
+        os.system("echo '{}' | kubectl apply -f - | sed 's/^/*** k8s: /g'".format(_privileges))
+        os.system("echo '{}' | kubectl apply -f - | sed 's/^/*** k8s: /g'".format(_secret))
 
 
 if __name__ == "__main__":
@@ -79,11 +107,34 @@ if __name__ == "__main__":
     _template_rolebinding_path = 'role_binding_template.yaml'
     _template_regcred_path = 'regcred_template.yaml'
 
-    _keycloak_handler = KeycloakHandler(_args.keycloak_user, _args.keycloak_pass, "k8s")
-    _kubernetes_handler = KubernetesHandler(_template_namespace_path, _template_rolebinding_path, _template_regcred_path)
+    # Parse the input CSV file
+    try:
+        _parsed_csv_file = pd.read_csv(_args.csv_file)
+    except FileNotFoundError:
+        sys.stderr.write("Input CSV file ('{}') does not exist. Abort\n".format(_args.csv_file))
+        sys.exit(1)
+    except pd.errors.ParserError:
+        sys.stderr.write("Impossible to parse CSV file ('{}'). Abort\n".format(_args.csv_file))
+        sys.exit(1)
 
-    parsed_csv_file = pd.read_csv(_args.csv_file, sep=",")
-    for _, student in parsed_csv_file.iterrows():
+    # Establish a connection with Keycloak
+    try:
+        _keycloak_handler = KeycloakHandler(_args.keycloak_user, _args.keycloak_pass, "k8s")
+    except kc.exceptions.KeycloakAuthenticationError:
+        sys.stderr.write("Invalid admin credentials (user: {}). Abort\n".format(_args.keycloak_user))
+        sys.exit(1)
+    except KeycloakHandlerException as _ex:
+        sys.stderr.write("An error has occurred: {}. Abort\n".format(_ex))
+        sys.exit(1)
+
+    # Prepare the templates for the resources to create in Kubernetes
+    try:
+        _kubernetes_handler = KubernetesHandler(_template_namespace_path, _template_rolebinding_path, _template_regcred_path)
+    except Template.exceptions.TemplateNotFound as _ex:
+        sys.stderr.write("Failed to parse the Jinja2 template: '{}' does not exist. Abort\n".format(_ex))
+        sys.exit(1)
+
+    for _, student in _parsed_csv_file.iterrows():
 
         _username = student['Username']
         _email = student['Email']
@@ -91,19 +142,27 @@ if __name__ == "__main__":
         _first_name = student['First name']
         _course_code = student['Course (code)']
 
-        _keycloak_handler.add_client_role(_course_code)
+        sys.stdout.write("Processing user {} ({} {}) - Course: {}\n".format(_username, _first_name, _last_name, _course_code))
+
+        # Grab the information about the user from Keycloak
         _user = _keycloak_handler.get_user(_username)
 
-        if len(_user) == 0:
-            # Create a new user
-            print("\nProcessing user {} ({} {}) enrolled in {}".format(_username, _first_name, _last_name, _course_code))
-            _keycloak_handler.create_new_user(_email, _username, _first_name, _last_name, _course_code)
-            # Create the Kubernetes resources
-            _kubernetes_handler.create_resources(_username, _course_code)
-
+        # The user does not yet exist
+        if _user is None:
+            # Create a new user in Keycloak (automatically skipped if the user already exists)
+            sys.stdout.write("* Creating user {} in Keycloak\n".format(_username))
+            _user = _keycloak_handler.create_new_user(_email, _username, _first_name, _last_name)
         else:
-            # Update course_code
-            print("\nUpdating user {} ({} {}) enrolled in {}".format(_username, _first_name, _last_name, _course_code))
-            _keycloak_handler.add_user_to_course(_user[0], _username, _course_code)
-        
-        _keycloak_handler.add_user_role(_username,_course_code)
+            sys.stdout.write("* User {} already exists in Keycloak\n".format(_username))
+
+        sys.stdout.write("* Ensuring course {} exists in Keycloak\n".format(_course_code))
+        _keycloak_handler.add_client_role(_course_code)
+
+        sys.stdout.write("* Assigning user {} to course {}\n".format(_username, _course_code))
+        _keycloak_handler.add_user_to_course(_user, _course_code)
+
+        # Create the Kubernetes resources
+        sys.stdout.write("* Creating Kubernetes resources for user {}\n".format(_username))
+        _kubernetes_handler.create_resources(_username, _course_code)
+
+        sys.stdout.write('\n')
