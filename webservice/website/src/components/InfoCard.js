1162cfbc3e0e50d105e1d09bb5ca2af9e1e78009
Simone Magnani <simonemagnani.96@gmail.com>
2020-03-22 00:11:20 +0100

    Added states and event management to correctly color instances in menu

--- webservice/website/src/components/InfoCard.js
+++ webservice/website/src/components/InfoCard.js
@@ -1,4 +1,4 @@
-import {Card, Col} from "react-bootstrap";
+import {Button, Card} from "react-bootstrap";
 import React from "react";
 
 /**
@@ -7,17 +7,22 @@ import React from "react";
  * @return the object to be drawn
  */
 export default function InfoCard(props) {
-    return <Col className="col-2 text-center">
-        <Card className="my-5 p-2 text-center text-dark" border="dark"
+    /*Retrieving instance labs to be drawn in the right bar and foreach one draw a button*/
+    const toDraw = Array.from(props.runningLabs.keys()).map(x => {
+        let status = props.runningLabs.get(x) ? props.runningLabs.get(x).status : -1;
+        let color = status === 0 ? 'orange' : status === 1 ? 'green' : 'red';
+        return <Button key={x} variant="link" style={{color: color}}
+                       onClick={() => props.func(x, null)}>{x}</Button>;
+    });
+    return <Card className="my-5 p-2 text-center text-dark" border="dark"
               style={{backgroundColor: 'transparent'}}>
             <Card.Body>
                 <Card.Title className="p-2">Details</Card.Title>
                 <p>Selected Lab</p>
                 <p className="text-primary">{props.selectedCRD || "-"}</p>
                 <p>Running Labs</p>
-                <p className="text-success">{props.runningLabs.length > 0 ? "" : "-"}</p>
-                {props.runningLabs}
+                <p className="text-success">{props.runningLabs.size > 0 ? "" : "-"}</p>
+                {toDraw}
             </Card.Body>
-        </Card>
-    </Col>;
+        </Card>;
 }
