82aa2c99ecd101a80acf8d7f35a91f5ef7dd4103
s41m0n <simonemagnani.96@gmail.com>
2020-03-23 22:16:28 +0100

    Added check to render provileged user page

--- webservice/website/src/components/Header.js
+++ webservice/website/src/components/Header.js
@@ -9,10 +9,15 @@ import NavItem from "react-bootstrap/NavItem";
  */
 export default function Header(props) {
     const toDraw = props.logged? <Button variant="outline-light" onClick={props.logout}>Logout</Button> : <img src={require('../assets/logo_poli3.png')} height="50px" alt=""/>;
+    const name = props.adminHidden ? "Professor Area" : "Student Area";
+    const adminBtn = props.renderAdminBtn ? <Button variant="outline-light" onClick={props.switchAdminView}>{name}</Button> : <div/>;
     return <header>
         <Navbar bg="dark" variant="dark" expand="lg">
             <Navbar.Brand href="">CrownLabs</Navbar.Brand>
             <Nav className="ml-auto" as="ul">
+                <NavItem as="li" className="mr-2">
+                    {adminBtn}
+                </NavItem>
                 <NavItem as="li">
                     {toDraw}
                 </NavItem>
