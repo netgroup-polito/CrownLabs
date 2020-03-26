096c7917d1fb11a84aa3b3e678a2b71c9f6c36c3
Francesco <francesco.borgogni4@gmail.com>
2020-03-26 17:30:23 +0100

    fixed redirect urls

--- operators/labInstance-operator/pkg/creation.go
+++ operators/labInstance-operator/pkg/creation.go
@@ -126,8 +126,8 @@ func CreateIngress(name string, namespace string, svc corev1.Service) v1beta1.In
 				"nginx.ingress.kubernetes.io/rewrite-target":     "/$2",
 				"nginx.ingress.kubernetes.io/proxy-read-timeout": "3600",
 				"nginx.ingress.kubernetes.io/proxy-send-timeout": "3600",
-				"nginx.ingress.kubernetes.io/auth-signin":        "https://$host/" + urlUUID + "/oauth2/start?rd=$escaped_request_uri",
-				"nginx.ingress.kubernetes.io/auth-url":           "https://$host/" + urlUUID + "/oauth2/auth",
+				"nginx.ingress.kubernetes.io/auth-signin":        "https://$host/" + name + "/oauth2/start?rd=$escaped_request_uri",
+				"nginx.ingress.kubernetes.io/auth-url":           "https://$host/" + name + "/oauth2/auth",
 				"crownlabs.polito.it/probe-url":                  url,
 			},
 		},
@@ -248,7 +248,7 @@ func CreateOauth2Ingress(name string, namespace string, svc corev1.Service) v1be
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name + "-oauth2-ingress",
 			Namespace: namespace,
-			Labels:    map[string]string{"cert-manager.io/cluster-issuer": "letsencrypt-production"},
+			Annotations:    map[string]string{"cert-manager.io/cluster-issuer": "letsencrypt-production"},
 		},
 		Spec: v1beta1.IngressSpec{
 			TLS: []v1beta1.IngressTLS{
