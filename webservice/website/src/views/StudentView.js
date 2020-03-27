dd2893f7e404c21cdcaf9a8cc6a50ddb44eaaf41
Giuseppe Ognibene <ognibenekaido@gmail.com>
2020-03-27 11:26:21 +0100

    new scrollbar, new buttons (#110)

--- webservice/website/src/views/StudentView.js
+++ webservice/website/src/views/StudentView.js
@@ -7,18 +7,16 @@ import StatusArea from "../components/StatusArea";
 export default function StudentView(props) {
     return <div style={{minHeight: '100vh'}}>
         <Row className="mt-5 p-3">
-            <Col className="col-2"/>
-            <Col className="col-4">
+            <Col className="col-6">
                 <LabTemplatesList labs={props.templateLabs} func={props.funcTemplate}
                                   start={props.start}/>
             </Col>
-            <Col className="col-4">
+            <Col className="col-6">
                 <LabInstancesList runningLabs={props.instanceLabs}
                                   func={props.funcInstance} connect={props.connect}
                                   stop={props.stop}
                                   showStatus={props.showStatus}/>
             </Col>
-            <Col className="col-2"/>
         </Row>
         <Row>
             <Col className="col-2"/>
