a87aa973f36e090e12698a6612549d40842c0aca
Gabriele Filaferro <gabriele.filaferro@gmail.com>
2020-04-07 22:18:09 +0200

    Removed a bit of bootstrap from the labPapers part of StudentView and styled it in order to make the content fill the total height

--- webservice/website/src/views/StudentView.js
+++ webservice/website/src/views/StudentView.js
@@ -4,35 +4,46 @@ import LabInstancesList from '../components/LabInstancesList';
 import React from 'react';
 import StatusArea from '../components/StatusArea';
 import './admin.css';
+import { makeStyles } from 'material-ui-core/styles';
+
+const useStyles = makeStyles(theme => ({
+  labPapers: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    width: '100%',
+    flexWrap: 'wrap',
+    marginTop: 30
+  }
+}));
 
 export default function StudentView(props) {
+  const classes = useStyles();
+
   return (
-    <div>
-      <Row>
-        <Col>
-          <LabTemplatesList
-            labs={props.templateLabs}
-            func={props.funcTemplate}
-            start={props.start}
-          />
-        </Col>
-        <Col>
-          <LabInstancesList
-            runningLabs={props.instanceLabs}
-            func={props.funcInstance}
-            connect={props.connect}
-            stop={props.stop}
-            showStatus={props.showStatus}
-          />
-        </Col>
-      </Row>
-      <Row className="text-center">
-        <Col />
-        <Col className="col-8">
-          <StatusArea hidden={props.hidden} events={props.events} />
-        </Col>
-        <Col />
-      </Row>
-    </div>
+    <>
+      <div className={classes.labPapers}>
+        <LabTemplatesList
+          labs={props.templateLabs}
+          func={props.funcTemplate}
+          start={props.start}
+        />
+        <LabInstancesList
+          runningLabs={props.instanceLabs}
+          func={props.funcInstance}
+          connect={props.connect}
+          stop={props.stop}
+          showStatus={props.showStatus}
+        />
+      </div>
+      <div>
+        <Row className="text-center">
+          <Col />
+          <Col className="col-8">
+            <StatusArea hidden={props.hidden} events={props.events} />
+          </Col>
+          <Col />
+        </Row>
+      </div>
+    </>
   );
 }
