a55f57664fbea995d3a1862aa31de684b5d9a385
davidefalcone1 <davidefalcone1@gmail.com>
2021-01-05 18:59:46 +0100

    instance_creation unit test: exposition.go tested

--- operators/pkg/instance-creation/cloud-init_test.go
+++ operators/pkg/instance-creation/cloud-init_test.go
@@ -1,12 +1,51 @@
 package instance_creation
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/yaml.v2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+func TestCreateUserData(t *testing.T) {
+	var (
+		nextUsername     = "usertest"
+		nextPassword     = "passtest"
+		nextCloudBaseUrl = "nextcloud.url"
+	)
+	publicKeys := []string{"key1", "key2", "key3"}
+
+	rawConfig := createUserdata(nextUsername, nextPassword, nextCloudBaseUrl, publicKeys)
+
+	var config cloudInitConfig
+
+	err := yaml.Unmarshal([]byte(rawConfig["userdata"]), &config)
+
+	assert.Equal(t, err, nil, "Yaml parser should return nil error.")
+
+	// check if header comment is present
+	hc := strings.HasPrefix(rawConfig["userdata"], "#cloud-config\n")
+
+	var (
+		expectedmount       = []string{nextCloudBaseUrl + "/remote.php/dav/files/" + nextUsername, "/media/MyDrive", "davfs", "_netdev,auto,user,rw,uid=1000,gid=1000", "0", "0"}
+		expectedcontent     = "/media/MyDrive " + nextUsername + " " + nextPassword
+		expectedpath        = "/etc/davfs2/secrets"
+		expectedpermissions = "0600"
+	)
+	assert.Equal(t, hc, true, "Cloud-init head comment should be present.")
+	assert.Equal(t, config.Network.Version, 2, "Network version should be set to 2.")
+	assert.Equal(t, config.Network.ID0.Dhcp4, true, "DHCPv4 should be set to true.")
+	assert.Equal(t, config.Mounts[0], expectedmount, "Nextcloud mount should be set to "+strings.Join(expectedmount, ", ")+".")
+	assert.Equal(t, config.WriteFiles[0].Content, expectedcontent, "Nextcloud secret should be se to "+expectedcontent+" .")
+	assert.Equal(t, config.WriteFiles[0].Path, expectedpath, "Nextcloud secret path should be set to "+expectedpath+".")
+	assert.Equal(t, config.WriteFiles[0].Permissions, expectedpermissions, "Nextcloud secret permissions should be set to "+expectedpermissions+" .")
+	assert.Equal(t, config.SSHAuthorizedKeys[0], publicKeys[0], "Public key should be set to"+publicKeys[0]+" .")
+	assert.Equal(t, config.SSHAuthorizedKeys[1], publicKeys[1], "Public key should be set to"+publicKeys[1]+" .")
+	assert.Equal(t, config.SSHAuthorizedKeys[2], publicKeys[2], "Public key should be set to"+publicKeys[2]+" .")
+
+}
 
 func TestCreateCloudInitSecret(t *testing.T) {
 	var (
--- operators/pkg/instance-creation/creation_test.go
+++ operators/pkg/instance-creation/creation_test.go
@@ -2,14 +2,12 @@ package instance_creation
 
 import (
 	"strconv"
-	"strings"
 	"testing"
 
 	"github.com/netgroup-polito/CrownLabs/operators/api/v1alpha2"
 	"k8s.io/apimachinery/pkg/api/resource"
 
 	"github.com/stretchr/testify/assert"
-	"gopkg.in/yaml.v2"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -47,44 +45,6 @@ func TestWhitelist(t *testing.T) {
 	c2 := CheckLabels(ns2, labels)
 	assert.Equal(t, c1, true, "The two label set should be identical and return true.")
 	assert.Equal(t, c2, false, "The two labels set should be different and return false.")
-}
-
-func TestCreateUserData(t *testing.T) {
-	var (
-		nextUsername     = "usertest"
-		nextPassword     = "passtest"
-		nextCloudBaseUrl = "nextcloud.url"
-	)
-	publicKeys := []string{"key1", "key2", "key3"}
-
-	rawConfig := createUserdata(nextUsername, nextPassword, nextCloudBaseUrl, publicKeys)
-
-	var config cloudInitConfig
-
-	err := yaml.Unmarshal([]byte(rawConfig["userdata"]), &config)
-
-	assert.Equal(t, err, nil, "Yaml parser should return nil error.")
-
-	// check if header comment is present
-	hc := strings.HasPrefix(rawConfig["userdata"], "#cloud-config\n")
-
-	var (
-		expectedmount       = []string{nextCloudBaseUrl + "/remote.php/dav/files/" + nextUsername, "/media/MyDrive", "davfs", "_netdev,auto,user,rw,uid=1000,gid=1000", "0", "0"}
-		expectedcontent     = "/media/MyDrive " + nextUsername + " " + nextPassword
-		expectedpath        = "/etc/davfs2/secrets"
-		expectedpermissions = "0600"
-	)
-	assert.Equal(t, hc, true, "Cloud-init head comment should be present.")
-	assert.Equal(t, config.Network.Version, 2, "Network version should be set to 2.")
-	assert.Equal(t, config.Network.ID0.Dhcp4, true, "DHCPv4 should be set to true.")
-	assert.Equal(t, config.Mounts[0], expectedmount, "Nextcloud mount should be set to "+strings.Join(expectedmount, ", ")+".")
-	assert.Equal(t, config.WriteFiles[0].Content, expectedcontent, "Nextcloud secret should be se to "+expectedcontent+" .")
-	assert.Equal(t, config.WriteFiles[0].Path, expectedpath, "Nextcloud secret path should be set to "+expectedpath+".")
-	assert.Equal(t, config.WriteFiles[0].Permissions, expectedpermissions, "Nextcloud secret permissions should be set to "+expectedpermissions+" .")
-	assert.Equal(t, config.SSHAuthorizedKeys[0], publicKeys[0], "Public key should be set to"+publicKeys[0]+" .")
-	assert.Equal(t, config.SSHAuthorizedKeys[1], publicKeys[1], "Public key should be set to"+publicKeys[1]+" .")
-	assert.Equal(t, config.SSHAuthorizedKeys[2], publicKeys[2], "Public key should be set to"+publicKeys[2]+" .")
-
 }
 
 func TestCreateVirtualMachineInstance(t *testing.T) {
