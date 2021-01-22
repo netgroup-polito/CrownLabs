cc2a2d6b5ad4481d05badc58e7378d997ccea5ae
Gabriele Filaferro <gabriele.filaferro@gmail.com>
2021-01-22 12:08:27 +0100

    Removed code and references of the old front-end

--- webservice/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -eu
-./generate_config_js.sh >/usr/share/nginx/html/config.js
-
-sed -i".old" 's/<body>/<body><script type="text\/javascript" src="\/config.js"><\/script>/' /usr/share/nginx/html/index.html
-
-nginx -g "daemon off;"
