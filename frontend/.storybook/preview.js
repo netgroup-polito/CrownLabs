8c309737f353a091c082d0c3164b1e48bf93397d
Gabriele Filaferro <gabriele.filaferro@gmail.com>
2021-05-14 16:49:04 +0200

    Changed default viewport sizes

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,5 +1,22 @@
 import '../src/theming';
 
+const customViewports = {
+  pixel2XL: {
+    name: 'Mobile',
+    styles: {
+      width: '411px',
+      height: '823px',
+    },
+  },
+  iPad: {
+    name: 'Tablet',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -14,5 +31,10 @@ export const parameters = {
       { name: 'light', class: 'light', color: '#fafafa' },
       { name: 'dark', class: 'dark', color: '#1a1a1a' },
     ],
+  },
+  viewport: {
+    viewports: {
+      ...customViewports,
+    },
   },
 };
