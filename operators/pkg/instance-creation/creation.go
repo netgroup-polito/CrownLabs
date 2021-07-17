37778fef740de18cac45bf0eaa02804ebe79f17b
Marco Iorio <marco.iorio@polito.it>
2021-07-17 19:06:10 +0200

    Instance operator: delete deprecated custom CreateOrUpdate function

--- operators/pkg/instance-creation/creation.go
+++ operators/pkg/instance-creation/creation.go
@@ -3,19 +3,12 @@
 package instance_creation
 
 import (
-	"context"
 	"fmt"
 
-	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	networkingv1 "k8s.io/api/networking/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
-	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/klog/v2"
 	virtv1 "kubevirt.io/client-go/api/v1"
 	cdiv1 "kubevirt.io/containerized-data-importer/pkg/apis/core/v1alpha1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	crownlabsv1alpha2 "github.com/netgroup-polito/CrownLabs/operators/api/v1alpha2"
 )
@@ -190,105 +183,4 @@ func computeCPULimits(cpu uint32, hypervisorCoefficient float32) string {
 
 func computeCPURequests(cpu, percentage uint32) string {
 	return fmt.Sprintf("%f", float32(cpu*percentage)/100)
-}
-
-// CreateOrUpdate creates a resource or updates it if already exists.
-// Deprecated: use https://pkg.go.dev/sigs.k8s.io/controller-runtime/pkg/controller/controllerutil#CreateOrUpdate
-func CreateOrUpdate(ctx context.Context, c client.Client, object interface{}) error {
-	switch obj := object.(type) {
-	case corev1.Secret:
-		var sec corev1.Secret
-		err := c.Get(ctx, types.NamespacedName{
-			Namespace: obj.Namespace,
-			Name:      obj.Name,
-		}, &sec)
-		if err != nil {
-			err = c.Create(ctx, &obj, &client.CreateOptions{})
-			if err != nil && !errors.IsAlreadyExists(err) {
-				klog.Error("Unable to create secret " + obj.Name)
-				klog.Error(err)
-				return err
-			}
-		}
-	case corev1.PersistentVolumeClaim:
-		var pvc corev1.PersistentVolumeClaim
-		err := c.Get(ctx, types.NamespacedName{
-			Namespace: obj.Namespace,
-			Name:      obj.Name,
-		}, &pvc)
-		if err != nil {
-			err = c.Create(ctx, &obj, &client.CreateOptions{})
-			if err != nil && !errors.IsAlreadyExists(err) {
-				klog.Error("Unable to create pvc " + obj.Name)
-				klog.Error(err)
-				return err
-			}
-		} else {
-			return errors.NewBadRequest("ALREADY EXISTS")
-		}
-	case corev1.Service:
-		var svc corev1.Service
-		err := c.Get(ctx, types.NamespacedName{
-			Namespace: obj.Namespace,
-			Name:      obj.Name,
-		}, &svc)
-		if err != nil {
-			err = c.Create(ctx, &obj, &client.CreateOptions{})
-			if err != nil && !errors.IsAlreadyExists(err) {
-				klog.Error("Unable to create service " + obj.Name)
-				klog.Error(err)
-				return err
-			}
-		}
-	case networkingv1.Ingress:
-		var ing networkingv1.Ingress
-		err := c.Get(ctx, types.NamespacedName{
-			Namespace: obj.Namespace,
-			Name:      obj.Name,
-		}, &ing)
-		if err != nil && errors.IsNotFound(err) {
-			err = c.Create(ctx, &obj, &client.CreateOptions{})
-			if err != nil && !errors.IsAlreadyExists(err) {
-				klog.Error("Unable to create Ingress " + obj.Name)
-				klog.Error(err)
-			}
-		} else {
-			klog.Error("Unable to create an Ingress " + obj.Name)
-			klog.Error(err)
-			return err
-		}
-
-	case appsv1.Deployment:
-		var deploy appsv1.Deployment
-		err := c.Get(ctx, types.NamespacedName{
-			Namespace: obj.Namespace,
-			Name:      obj.Name,
-		}, &deploy)
-		if err != nil {
-			err = c.Create(ctx, &obj, &client.CreateOptions{})
-			if err != nil && !errors.IsAlreadyExists(err) {
-				klog.Error("Unable to create deployment " + obj.Name)
-				klog.Error(err)
-				return err
-			}
-		} else {
-			err = c.Update(ctx, &obj, &client.UpdateOptions{})
-			if err != nil {
-				klog.Error("unable to update deployment " + obj.Name)
-				klog.Error(err)
-				return err
-			}
-		}
-	case *virtv1.VirtualMachineInstance:
-		err := c.Create(ctx, obj, &client.CreateOptions{})
-		if err != nil && !errors.IsAlreadyExists(err) {
-			klog.Error("Unable to create virtual machine " + obj.Name)
-			klog.Error(err)
-			return err
-		}
-	default:
-		return errors.NewBadRequest("No matching type for requested resource:")
-	}
-
-	return nil
 }
