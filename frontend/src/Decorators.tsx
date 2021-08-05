edd6a54ebcd1756ca33d90f49853af41b13d3513
cheina97 <cheinasso.francesco@gmail.com>
2021-08-05 15:48:55 +0200

    Added Navbar component
    Co-authored-by: Alessandro Cannarella <cannarella.dev@gmail.com>

--- frontend/src/Decorators.tsx
+++ frontend/src/Decorators.tsx
@@ -5,6 +5,8 @@ import { Layout, Skeleton } from 'antd';
 import Button from 'antd-button-color';
 import { StoryContext } from '@storybook/react/dist/ts3.9/client/preview/types';
 import { Story } from '@storybook/react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import ThemeContextProvider from './contexts/ThemeContext';
 
 const ModalDecorator = (Story: Story, context: StoryContext) => {
   const [show, setShow] = useState(false);
@@ -39,4 +41,23 @@ const LogoDecorator = (Story: Story) => (
   </div>
 );
 
-export { ModalDecorator, LogoDecorator };
+const NavbarDecorator = (Story: Story) => (
+  <MemoryRouter>
+    <ThemeContextProvider>
+      <Route path="/">
+        <Layout>
+          <Story />
+          <Layout.Content>
+            <div className="m-4">
+              <Skeleton />
+              <Skeleton />
+              <Skeleton />
+            </div>
+          </Layout.Content>
+        </Layout>
+      </Route>
+    </ThemeContextProvider>
+  </MemoryRouter>
+);
+
+export { ModalDecorator, NavbarDecorator, LogoDecorator };
