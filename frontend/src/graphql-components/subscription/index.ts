bcef8dc87e623165a4d9835912f30a057bb0bb75
Marco Manco <manco.mrc@gamil.com>
2021-09-11 11:57:47 +0200

    feat: add mutation, subscription and setup for subscription, update of generate-types

---
+++ frontend/src/graphql-components/subscription/index.ts
@@ -0,0 +1,4 @@
+export { default as updatedOwnedInstances } from './instances.subscription';
+export { default as updatedSshKeys } from './key.subscription';
+export { default as updatedWorkspaceTemplates } from './templates.subscription';
+export { default as updatedTenant } from './tenant.subscription';
---
+++ frontend/src/graphql-components/subscription/tenant.subscription.ts
@@ -0,0 +1,33 @@
+import gql from 'graphql-tag';
+
+export default gql`
+  subscription updatedTenant($tenantId: String!) {
+    updatedTenant: itPolitoCrownlabsV1alpha1TenantUpdate(namespace: $tenantId) {
+      tenant: payload {
+        spec {
+          email
+          firstName
+          lastName
+          workspaces {
+            role
+            workspaceRef {
+              workspaceId: name
+              workspaceWrapper {
+                itPolitoCrownlabsV1alpha1Workspace {
+                  spec {
+                    workspaceName: prettyName
+                  }
+                  status {
+                    namespace {
+                      workspaceNamespace: name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
