5d3e6bae405fa021420f0ae628b8ef092803d7a1
qcfe <io@qcfe.me>
2022-02-11 16:00:10 +0100

    Content uploader, force 200 <= http_code < 300

--- provisioning/containers/utils/content-uploader/entrypoint.sh
+++ provisioning/containers/utils/content-uploader/entrypoint.sh
@@ -20,4 +20,10 @@ cd "$SOURCE_PATH"
 zip "/tmp/$FILENAME.zip" -r .
 
 echo "Uploading archive..."
-curl -v --fail --request POST --form "binfile=@\"/tmp/$FILENAME.zip\"" --form "filename=$FILENAME.zip" "$DESTINATION_URL"
+HTTP_CODE=$(curl -v --request --fail POST --form "binfile=@\"/tmp/$FILENAME.zip\"" --form "filename=$FILENAME.zip" "$DESTINATION_URL" --write-out "%{http_code}")
+
+# return non zero if HTTP_CODE is less than 200 or greater equal than 300
+if [ "$HTTP_CODE" -lt "200" ] || [ "$HTTP_CODE" -ge "300" ]; then
+  echo "Upload failed with HTTP code $HTTP_CODE"
+  exit 1
+fi
