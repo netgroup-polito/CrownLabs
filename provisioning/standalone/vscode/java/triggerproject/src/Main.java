25e3d65b362ee90df2d1f2f5d7a498f2e6fe7676
QcFe <federico.cucinella@polito.it>
2024-03-05 16:04:24 +0100

    add vscode-java & upgrade vscode-server
    Co-authored-by: Marco Torchiano <marco.torchiano@polito.it>

---
+++ provisioning/standalone/vscode/java/triggerproject/src/Main.java
@@ -0,0 +1,5 @@
+public class Main{
+    public static int m(){
+        return 1;
+    }
+}
---
+++ provisioning/standalone/vscode/java/triggerproject/test/MainTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void test(){
+       assertEquals(1,Main.m()); 
+    }
+}
