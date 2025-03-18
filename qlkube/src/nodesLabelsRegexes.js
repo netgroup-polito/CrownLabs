4a4ced3cf334f2edcf0996129c96a8fcbd23f9d4
loris-catalano <161748350+loris-catalano@users.noreply.github.com>
2025-03-18 18:10:54 +0100

    add new function to GraphQL to retrieve nodes labels

---
+++ qlkube/src/nodesLabelsRegexes.js
@@ -0,0 +1,9 @@
+// This file serves development/testing purposes only.
+// When qlkube is deployed with Helm, it is overwritten by an equivalent
+// one automatically generated from the configuration therein specified.
+
+const regexes = [
+    /crownlabs\.polito\.it.+/,
+];
+
+module.exports = { regexes };
