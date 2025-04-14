e23b49cdb5c9f3b46b55237db6cd66fc2bbd81b2
Loris Catalano <loriscatalano8@gmail.com>
2025-04-14 13:23:28 +0000

    add missing component

---
+++ frontend/src/components/common/NodeSelectorIcon/NodeSelectorIcon.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+import { Tooltip } from 'antd';
+
+import { AimOutlined } from '@ant-design/icons';
+import { cleanupLabels } from '../../../utils';
+
+export interface INodeSelectorIconProps {
+  nodeSelector: JSON;
+  isOnWorkspace: boolean;
+}
+
+const NodeSelectorIcon: FC<INodeSelectorIconProps> = ({ ...props }) => {
+  const { nodeSelector, isOnWorkspace } = props;
+
+  const displaySel = Object.entries(nodeSelector)
+    .map(([k, v]) => `${cleanupLabels(k)}=${v}`)
+    .join(',');
+
+  const tooltipText = !isOnWorkspace
+    ? `This instance started on a node with ${displaySel}`
+    : displaySel
+    ? `This instance will be started on nodes with ${displaySel}`
+    : 'This instance can be started choosing the target node';
+
+  return (
+    <Tooltip title={<div className="text-center">{tooltipText}</div>}>
+      <div className="primary-color-fg flex items-center">
+        <AimOutlined width="22px" />
+      </div>
+    </Tooltip>
+  );
+};
+
+export default NodeSelectorIcon;
