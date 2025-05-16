271986466ca64833f83611d4161672f509a16681
Andrea Buonaurio <65712954+andreabuon@users.noreply.github.com>
2025-05-16 11:49:00 +0200

    refactor: extract instance automation

--- operators/cmd/instance-automation/main.go
+++ operators/cmd/instance-automation/main.go
@@ -12,13 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package main contains the entrypoint for the instance operator.
+// Package main contains the entrypoint for the instance automation operator.
 package main
 
 import (
 	"flag"
 	"os"
 	"strings"
+	"time"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -36,9 +37,8 @@ import (
 	crownlabsv1alpha1 "github.com/netgroup-polito/CrownLabs/operators/api/v1alpha1"
 	crownlabsv1alpha2 "github.com/netgroup-polito/CrownLabs/operators/api/v1alpha2"
 	"github.com/netgroup-polito/CrownLabs/operators/pkg/forge"
-	instancesnapshot_controller "github.com/netgroup-polito/CrownLabs/operators/pkg/instancesnapshot-controller"
+	"github.com/netgroup-polito/CrownLabs/operators/pkg/instautoctrl"
 	"github.com/netgroup-polito/CrownLabs/operators/pkg/instctrl"
-	"github.com/netgroup-polito/CrownLabs/operators/pkg/shvolctrl"
 	"github.com/netgroup-polito/CrownLabs/operators/pkg/utils/restcfg"
 )
 
@@ -59,20 +59,23 @@ func init() {
 func main() {
 	containerEnvOpts := forge.ContainerEnvOpts{}
 	svcUrls := instctrl.ServiceUrls{}
-	instSnapOpts := instancesnapshot_controller.ContainersSnapshotOpts{}
 
 	metricsAddr := flag.String("metrics-addr", ":8080", "The address the metric endpoint binds to.")
 	enableLeaderElection := flag.Bool("enable-leader-election", false,
 		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
-	maxConcurrentReconciles := flag.Int("max-concurrent-reconciles", 1, "The maximum number of concurrent Reconciles which can be run for the Instance controller")
 
 	namespaceWhiteList := flag.String("namespace-whitelist", "production=true", "The whitelist of the namespaces on "+
 		"which the controller will work. Different labels (key=value) can be specified, by separating them with a &"+
 		"( e.g. key1=value1&key2=value2")
 
-	sharedVolumeStorageClass := flag.String("shared-volume-storage-class", "rook-nfs", "The StorageClass to be used for all SharedVolumes' PVC (if unique can be used to enforce ResourceQuota on Workspaces, about number and size of ShVols)")
-
+	maxConcurrentTerminationReconciles := flag.Int("max-concurrent-reconciles-termination", 1, "The maximum number of concurrent Reconciles which can be run for the Instance Termination controller")
+	instanceTerminationStatusCheckTimeout := flag.Duration("instance-termination-status-check-timeout", 3*time.Second, "The maximum time to wait for the status check for Instances that require it")
+	instanceTerminationStatusCheckInterval := flag.Duration("instance-termination-status-check-interval", 2*time.Minute, "The interval to check the status of Instances that require it")
 	maxConcurrentSubmissionReconciles := flag.Int("max-concurrent-reconciles-submission", 1, "The maximum number of concurrent Reconciles which can be run for the Instance Submission controller")
+
+	maxInactiveConcurrentTerminationReconciles := flag.Int("max-inactive-concurrent-reconciles-termination", 1, "The maximum number of concurrent Reconciles which can be run for the Instance Termination controller")
+	instanceInactiveTerminationStatusCheckTimeout := flag.Duration("instance-inactive-termination-status-check-timeout", 3*time.Second, "The maximum time to wait for the status check for Instances that require it")
+	instanceInactiveTerminationStatusCheckInterval := flag.Duration("instance-inactive-termination-status-check-interval", 2*time.Minute, "The interval to check the status of Instances that require it")
 
 	flag.StringVar(&svcUrls.WebsiteBaseURL, "website-base-url", "crownlabs.polito.it", "Base URL of crownlabs website instance")
 	flag.StringVar(&svcUrls.InstancesAuthURL, "instances-auth-url", "", "The base URL for user instances authentication (i.e., oauth2-proxy)")
@@ -83,12 +86,6 @@ func main() {
 	flag.StringVar(&containerEnvOpts.ContentDownloaderImg, "container-env-content-downloader-img", "latest", "The image name for the init-container to download and unarchive initial content to the instance volume.")
 	flag.StringVar(&containerEnvOpts.ContentUploaderImg, "container-env-content-uploader-img", "latest", "The image name for the job to compress and upload instance content from a persistent instance.")
 	flag.StringVar(&containerEnvOpts.InstMetricsEndpoint, "container-env-instmetrics-server-endpoint", "instmetrics:9090", "The endpoint of the InstMetrics gRPC server")
-
-	flag.StringVar(&instSnapOpts.VMRegistry, "vm-registry", "", "The registry where VMs should be uploaded")
-	flag.StringVar(&instSnapOpts.RegistrySecretName, "vm-registry-secret", "", "The name of the secret for the VM registry")
-
-	flag.StringVar(&instSnapOpts.ContainerImgExport, "container-export-img", "crownlabs/img-exporter", "The image for the img-exporter (container in charge of exporting the disk of a persistent vm)")
-	flag.StringVar(&instSnapOpts.ContainerKaniko, "container-kaniko-img", "gcr.io/kaniko-project/executor", "The image for the Kaniko container to be deployed")
 
 	restcfg.InitFlags(nil)
 	klog.InitFlags(nil)
@@ -117,42 +114,44 @@ func main() {
 
 	nsWhitelist := metav1.LabelSelector{MatchLabels: whiteListMap, MatchExpressions: []metav1.LabelSelectorRequirement{}}
 
-	// Configure the Instance controller
-	const instanceCtrlName = "Instance"
-	if err = (&instctrl.InstanceReconciler{
+	// Configure the Instance termination controller
+	instanceTermination := "InstanceTermination"
+	if err := (&instautoctrl.InstanceTerminationReconciler{
+		Client:                      mgr.GetClient(),
+		Scheme:                      mgr.GetScheme(),
+		EventsRecorder:              mgr.GetEventRecorderFor(instanceTermination),
+		NamespaceWhitelist:          nsWhitelist,
+		StatusCheckRequestTimeout:   *instanceTerminationStatusCheckTimeout,
+		InstanceStatusCheckInterval: *instanceTerminationStatusCheckInterval,
+	}).SetupWithManager(mgr, *maxConcurrentTerminationReconciles); err != nil {
+		log.Error(err, "unable to create controller", "controller", instanceTermination)
+		os.Exit(1)
+	}
+
+	// Configure the Instance submission controller
+	instanceSubmission := "InstanceSubmission"
+	if err := (&instautoctrl.InstanceSubmissionReconciler{
 		Client:             mgr.GetClient(),
 		Scheme:             mgr.GetScheme(),
-		EventsRecorder:     mgr.GetEventRecorderFor(instanceCtrlName),
-		NamespaceWhitelist: nsWhitelist,
-		ServiceUrls:        svcUrls,
+		EventsRecorder:     mgr.GetEventRecorderFor(instanceSubmission),
 		ContainerEnvOpts:   containerEnvOpts,
-	}).SetupWithManager(mgr, *maxConcurrentReconciles); err != nil {
-		log.Error(err, "unable to create controller", "controller", instanceCtrlName)
-		os.Exit(1)
-	}
-
-	// Configure the SharedVolume controller
-	const sharedVolumeCtrl = "SharedVolume"
-	if err := (&shvolctrl.SharedVolumeReconciler{
-		Client:             mgr.GetClient(),
-		EventsRecorder:     mgr.GetEventRecorderFor(sharedVolumeCtrl),
 		NamespaceWhitelist: nsWhitelist,
-		PVCStorageClass:    *sharedVolumeStorageClass,
 	}).SetupWithManager(mgr, *maxConcurrentSubmissionReconciles); err != nil {
-		log.Error(err, "unable to create controller", "controller", sharedVolumeCtrl)
+		log.Error(err, "unable to create controller", "controller", instanceSubmission)
 		os.Exit(1)
 	}
 
-	// Configure the InstanceSnapshot controller
-	instanceSnapshotCtrl := "InstanceSnapshot"
-	if err = (&instancesnapshot_controller.InstanceSnapshotReconciler{
-		Client:             mgr.GetClient(),
-		Scheme:             mgr.GetScheme(),
-		EventsRecorder:     mgr.GetEventRecorderFor(instanceSnapshotCtrl),
-		NamespaceWhitelist: nsWhitelist,
-		ContainersSnapshot: instSnapOpts,
-	}).SetupWithManager(mgr); err != nil {
-		log.Error(err, "unable to create controller", "controller", instanceSnapshotCtrl)
+	// Configure the Instance Inactive termination controller
+	instanceInactiveTermination := "InstanceInactiveTermination"
+	if err := (&instautoctrl.InstanceInactiveTerminationReconciler{
+		Client:                      mgr.GetClient(),
+		Scheme:                      mgr.GetScheme(),
+		EventsRecorder:              mgr.GetEventRecorderFor(instanceInactiveTermination),
+		NamespaceWhitelist:          nsWhitelist,
+		StatusCheckRequestTimeout:   *instanceInactiveTerminationStatusCheckTimeout,
+		InstanceStatusCheckInterval: *instanceInactiveTerminationStatusCheckInterval,
+	}).SetupWithManager(mgr, *maxInactiveConcurrentTerminationReconciles); err != nil {
+		log.Error(err, "unable to create controller", "controller", instanceInactiveTermination)
 		os.Exit(1)
 	}
 
