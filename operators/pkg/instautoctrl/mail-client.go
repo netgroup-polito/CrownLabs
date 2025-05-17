9bb9c260034fe2a30dfc79fb02d8b65378a668a7
Simone Tollardo <tollsimy.dev@protonmail.com>
2025-05-17 11:11:36 +0200

    refactor: mail client

--- operators/pkg/instautoctrl/mail-client.go
+++ operators/pkg/instautoctrl/mail-client.go
@@ -8,11 +8,11 @@ import (
 type MailClient struct {
 	SMTPServer string
 	SMTPPort   int
-	auth       smtp.Auth
+	Auth       smtp.Auth
 	From       string
 }
 
 func (m *MailClient) SendMail(to []string, subject string, body string) error {
 	msg := []byte("Subject: " + subject + "\n\n" + body)
-	return smtp.SendMail(m.SMTPServer+":"+strconv.Itoa(m.SMTPPort), m.auth, m.From, to, msg)
+	return smtp.SendMail(m.SMTPServer+":"+strconv.Itoa(m.SMTPPort), m.Auth, m.From, to, msg)
 }
