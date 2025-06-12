f12ea673044f78800f228ef0dab8f5543ed93135
Simone Tollardo <tollsimy.dev@protonmail.com>
2025-06-12 14:26:49 +0200

    Revert "removed unused args in instance-operator and respective deployment values"
    This reverts commit e4ae2cbd6f06115db6c7ed89a1dd27dcd7db412d.

--- operators/pkg/instctrl/controller.go
+++ operators/pkg/instctrl/controller.go
@@ -51,6 +51,7 @@ type InstanceReconciler struct {
 	EventsRecorder     record.EventRecorder
 	NamespaceWhitelist metav1.LabelSelector
 	ServiceUrls        ServiceUrls
+	ContainerEnvOpts   forge.ContainerEnvOpts
 
 	// This function, if configured, is deferred at the beginning of the Reconcile.
 	// Specifically, it is meant to be set to GinkgoRecover during the tests,
