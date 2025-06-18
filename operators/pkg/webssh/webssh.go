c7b01ab999f556a2ff24e15df0cf146f5b45a13f
lucam <lucam>
2025-06-18 08:38:36 +0000

    webssh implementation

--- operators/pkg/webssh/webssh.go
+++ operators/pkg/webssh/webssh.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -21,14 +22,69 @@ import (
 	"k8s.io/client-go/rest"
 )
 
-const (
-	sshUser            = "crownlabs"
-	privateKeyPath     = "/web-keys/ssh_web_bastion_master"
-	timeoutDuration    = 30 * time.Minute // Duration after which an SSH connection is considered idle and closed
-	maxIdleConnections = 1000             // Maximum number of idle SSH connections to keep open
-	vmPort             = "22"             // Default SSH port for VMs can be overridden in the ClientInitMessage
-	ServerPort         = "8080"           // Port on which the WebSocket server listens
-)
+type Config struct {
+	SSHUser            string
+	PrivateKeyPath     string
+	TimeoutDuration    int // Duration in seconds
+	MaxConnectionCount int
+	WebsocketPort      string
+	VmSSHPort          string // Default SSH port for VMs, can be overridden in the ClientInitMessage
+}
+
+func LoadConfig() *Config {
+
+	vmPort := os.Getenv("WEBSSH_VM_PORT")
+	if vmPort == "" {
+		vmPort = "22"
+		log.Println("WEBSSH_VM_PORT environment variable is not set, using default value: ", vmPort)
+	}
+
+	maxConnStr := os.Getenv("WEBSSH_MAX_CONN_COUNT")
+	if maxConnStr == "" {
+		maxConnStr = "1000" // Default value if not set
+		log.Println("WEBSSH_MAX_CONN_COUNT environment variable is not set, using default value: ", maxConnStr)
+	}
+
+	maxConn, err := strconv.Atoi(maxConnStr)
+	if err != nil {
+		maxConn = 1000
+		log.Println("WEBSSH_MAX_CONN_COUNT is not a valid integer, using default value: ", maxConn)
+	}
+
+	timeoutStr := os.Getenv("WEBSSH_TIMEOUT_DURATION")
+	timeout, err := strconv.Atoi(timeoutStr)
+	if err != nil {
+		timeout = 30
+		log.Println("WEBSSH_TIMEOUT_DURATION is not a valid integer, using default value: ", timeout)
+	}
+
+	SSHUser := os.Getenv("WEBSSH_USER")
+	if SSHUser == "" {
+		SSHUser = "crownlabs"
+		log.Println("WEBSSH_USER environment variable is not set, using default value: ", SSHUser)
+	}
+
+	privateKeyPath := os.Getenv("WEBSSH_PRIVATE_KEY_PATH")
+	if privateKeyPath == "" {
+		privateKeyPath = "/web-keys/ssh_web_bastion_master"
+		log.Println("WEBSSH_PRIVATE_KEY_PATH environment variable is not set, using default value: ", privateKeyPath)
+	}
+
+	websocketPort := os.Getenv("WEBSSH_WEBSOCKET_PORT")
+	if websocketPort == "" {
+		websocketPort = "8090"
+		log.Println("WEBSSH_WEBSOCKET_PORT environment variable is not set, using default value: ", websocketPort)
+	}
+
+	return &Config{
+		SSHUser:            SSHUser,
+		PrivateKeyPath:     privateKeyPath,
+		TimeoutDuration:    timeout * 60, // Convert minutes to seconds
+		MaxConnectionCount: maxConn,
+		WebsocketPort:      websocketPort,
+		VmSSHPort:          vmPort,
+	}
+}
 
 type sshConnInfo struct {
 	conn     *ssh.Client
@@ -103,24 +159,24 @@ func getInstances(token string, namespace string) (map[string]any, error) {
 	return instances, nil
 }
 
-func validateRequest(firstMsg []byte) (string, error) {
+func validateRequest(firstMsg []byte) (ClientInitMessage, error) {
 
 	var initMsg ClientInitMessage
 	if err := json.Unmarshal(firstMsg, &initMsg); err != nil {
 		log.Println("Invalid JSON from client:", err)
-		return "", errors.New("invalid JSON format")
+		return initMsg, errors.New("invalid JSON format")
 	}
 
 	if initMsg.VmIp == "" || initMsg.Token == "" {
 		log.Println("Missing required fields in the initialization message")
-		return "", errors.New("missing required fields in the initialization message")
+		return initMsg, errors.New("missing required fields in the initialization message")
 	}
 
 	// Extract username from the token
 	username, err := extractUsernameFromToken(initMsg.Token)
 	if err != nil {
 		log.Println("Token decode error:", err)
-		return "", errors.New("invalid token format")
+		return initMsg, errors.New("invalid token format")
 	}
 
 	namespace := "tenant-" + username
@@ -131,46 +187,49 @@ func validateRequest(firstMsg []byte) (string, error) {
 	instances, err := getInstances(initMsg.Token, namespace)
 	if err != nil {
 		log.Println("Error retrieving instances:", err)
-		return "", errors.New("error retrieving instances")
+		return initMsg, errors.New("error retrieving instances")
 	}
 
 	// check if the requested VM IP is in the list of instances
 	found := false
 	for _, i := range instances {
-		if instanceMap, ok := i.(map[string]any); ok {
-			if status, ok := instanceMap["status"].(map[string]any); ok {
-				if ip, ok := status["ip"].(string); ok {
-					log.Println("IP: ", ip)
-					if ip == initMsg.VmIp {
-						found = true
-						log.Printf("Found instance with IP: %s in namespace: %s", ip, namespace)
-						break // Exit the loop if we found the instance
-					}
-				}
-			}
+
+		instanceMap, ok := i.(map[string]any)
+		if !ok {
+			log.Println("Instance data is not in the expected format")
+			continue
+		}
+		status, ok := instanceMap["status"].(map[string]any)
+		if !ok {
+			continue
+		}
+
+		ip, ok := status["ip"].(string)
+		if !ok {
+			continue
+		}
+
+		log.Println("IP:", ip)
+
+		if ip == initMsg.VmIp {
+			found = true
+			log.Printf("Found instance with IP: %s in namespace: %s", ip, namespace)
+			break
 		}
 	}
 
 	if !found {
 		log.Printf("No instance found for VM IP: %s in namespace: %s", initMsg.VmIp, namespace)
-		return "", errors.New("no instance found for the provided VM IP")
+		return initMsg, errors.New("no instance found for the provided VM IP")
 	}
 
-	// ssh port used to connect to the VM
-	port := initMsg.VmPort
-	if port == "" {
-		port = vmPort // Default port if not specified
-	}
-
-	vmIp := initMsg.VmIp + ":" + port // ip:port
-
-	return vmIp, nil
+	return initMsg, nil
 }
 
-func loadPrivateKey() (ssh.Signer, error) {
-	key, err := os.ReadFile(privateKeyPath)
+func loadPrivateKey(path string) (ssh.Signer, error) {
+	key, err := os.ReadFile(path)
 	if err != nil {
-		log.Printf("Error reading private key file at %s: %v", privateKeyPath, err)
+		log.Printf("Error reading private key file at %s: %v", path, err)
 		return nil, err
 	}
 	return ssh.ParsePrivateKey(key)
@@ -204,7 +263,7 @@ func closeSSHConnection(vmIp string) {
 	}
 }
 
-func getOrCreateSSHConnection(vmIp string, sshConfig *ssh.ClientConfig) (*ssh.Client, error) {
+func getOrCreateSSHConnection(vmIp string, sshConfig *ssh.ClientConfig, maxConnCount int) (*ssh.Client, error) {
 	connMutex.Lock()
 	defer connMutex.Unlock()
 
@@ -216,9 +275,9 @@ func getOrCreateSSHConnection(vmIp string, sshConfig *ssh.ClientConfig) (*ssh.Cl
 	}
 
 	// check if the number of active connections exceeds the limit
-	if len(activeConnections) >= maxIdleConnections {
-		log.Printf("Maximum number of idle SSH connections (%d) reached. Cannot create new connection.", maxIdleConnections)
-		return nil, errors.New("maximum number of idle SSH connections reached")
+	if len(activeConnections) >= maxConnCount {
+		log.Printf("Maximum number of SSH connections (%d) reached. Cannot create new connection.", maxConnCount)
+		return nil, errors.New("maximum number of SSH connections reached")
 	}
 
 	// If not, create a new SSH connection
@@ -260,7 +319,8 @@ func startConnectionCleanup(interval time.Duration, timeout time.Duration) {
 		}
 	}()
 }
-func wsHandler(w http.ResponseWriter, r *http.Request) {
+
+func wsHandler(w http.ResponseWriter, r *http.Request, config *Config) {
 
 	// upgrade to the WebSocket
 	ws, err := upgrader.Upgrade(w, r, nil)
@@ -278,28 +338,34 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	_, firstMsg, err := ws.ReadMessage()
 	if err != nil {
 		log.Println("ReadMessage error:", err)
+		ws.WriteMessage(websocket.TextMessage, []byte("Error reading initial message"))
 		return
 	}
 
-	sshStr, err := validateRequest(firstMsg)
+	initMsg, err := validateRequest(firstMsg)
 	if err != nil {
 		log.Println("Request validation failed:", err)
 		ws.WriteMessage(websocket.TextMessage, []byte("Invalid request"))
-		ws.Close()
 		return
 	}
 
+	// Construct the SSH connection string
+	port := initMsg.VmPort
+	if port == "" {
+		port = config.VmSSHPort // Default port if not specified
+	}
+	connString := initMsg.VmIp + ":" + port // ip:port
+
 	// Load the private key for SSH authentication
-	signer, err := loadPrivateKey()
+	signer, err := loadPrivateKey(config.PrivateKeyPath)
 	if err != nil {
 		log.Println("Failed to load private key:", err)
-		ws.WriteMessage(websocket.TextMessage, []byte("Failed to load private key"))
-		ws.Close()
+		ws.WriteMessage(websocket.TextMessage, []byte("Interal server error"))
 		return
 	}
 
 	sshConfig := &ssh.ClientConfig{
-		User: sshUser,
+		User: config.SSHUser,
 		Auth: []ssh.AuthMethod{
 			ssh.PublicKeys(signer),
 		},
@@ -307,16 +373,17 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		Timeout:         10 * time.Second,
 	}
 
-	sshConn, err := getOrCreateSSHConnection(sshStr, sshConfig)
+	sshConn, err := getOrCreateSSHConnection(connString, sshConfig, config.MaxConnectionCount)
 	if err != nil {
 		log.Println("Failed to get or create SSH connection:", err)
-		http.Error(w, "Failed to connect to VM, too many connections", http.StatusInternalServerError)
+		ws.WriteMessage(websocket.TextMessage, []byte("Failed to connect to SSH server"))
 		return
 	}
 
 	session, err := sshConn.NewSession()
 	if err != nil {
 		log.Println("Failed to create SSH session:", err)
+		ws.WriteMessage(websocket.TextMessage, []byte("Internal server error"))
 		return
 	}
 	defer session.Close()
@@ -329,22 +396,26 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 
 	if err := session.RequestPty("xterm", 40, 80, modes); err != nil {
 		log.Println("Request for pseudo terminal failed:", err)
+		ws.WriteMessage(websocket.TextMessage, []byte("Internal server error"))
 		return
 	}
 
 	stdin, err := session.StdinPipe()
 	if err != nil {
 		log.Println("Unable to setup stdin for session:", err)
+		ws.WriteMessage(websocket.TextMessage, []byte("Internal server error"))
 		return
 	}
 	stdout, err := session.StdoutPipe()
 	if err != nil {
 		log.Println("Unable to setup stdout for session:", err)
+		ws.WriteMessage(websocket.TextMessage, []byte("Internal server error"))
 		return
 	}
 
 	if err := session.Shell(); err != nil {
 		log.Println("Failed to start shell:", err)
+		ws.WriteMessage(websocket.TextMessage, []byte("Internal server error"))
 		return
 	}
 
@@ -359,7 +430,7 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 				}
 				break
 			}
-			updateSSHConnectionLastUsed(sshStr) // reset timer on shell output
+			updateSSHConnectionLastUsed(connString) // reset timer on shell output
 			if err := ws.WriteMessage(websocket.TextMessage, buf[:n]); err != nil {
 				log.Println("WebSocket write error:", err)
 				break
@@ -372,10 +443,10 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		_, msg, err := ws.ReadMessage()
 		if err != nil {
 			log.Println("Closing SSH session, WebSocket read error:", err)
-			closeSSHConnection(sshStr)
+			closeSSHConnection(connString)
 			break
 		}
-		updateSSHConnectionLastUsed(sshStr) // reset timer on user input
+		updateSSHConnectionLastUsed(connString) // reset timer on user input
 		if _, err := stdin.Write(msg); err != nil {
 			log.Println("SSH stdin write error:", err)
 			break
@@ -383,13 +454,22 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func StartWebSSH() {
-	// automatic Cleanup
-	startConnectionCleanup(1*time.Minute, timeoutDuration)
+func wsHandlerWrapper(config *Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		wsHandler(w, r, config)
+	}
+}
 
-	http.HandleFunc("/ws", wsHandler)
-	log.Println("WebSocket SSH bridge running on :" + ServerPort)
-	err := http.ListenAndServe(":"+ServerPort, nil)
+func StartWebSSH() {
+	// Load configuration from environment variables
+	config := LoadConfig()
+
+	// automatic Cleanup
+	startConnectionCleanup(2*time.Minute, time.Duration(config.TimeoutDuration)*time.Second)
+
+	http.HandleFunc("/ws", wsHandlerWrapper(config))
+	log.Println("WebSocket SSH bridge running on :" + config.WebsocketPort)
+	err := http.ListenAndServe(":"+config.WebsocketPort, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
