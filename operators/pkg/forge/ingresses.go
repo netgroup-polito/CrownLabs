7b67bdb079966a1183211a28fb3d9ede2596996f
KUOHANTUNG <s325669@stuenti.polito.it>
2025-06-30 15:50:14 +0200

    clear some mistakes

--- operators/pkg/forge/ingresses.go
+++ operators/pkg/forge/ingresses.go
@@ -19,7 +19,6 @@ import (
 	"strings"
 
 	netv1 "k8s.io/api/networking/v1"
-	"k8s.io/utils/ptr"
 
 	clv1alpha2 "github.com/netgroup-polito/CrownLabs/operators/api/v1alpha2"
 )
@@ -30,8 +29,6 @@ const (
 
 	// IngressGUINameSuffix -> the suffix added to the name of the ingress targeting the environment GUI.
 	IngressGUINameSuffix = "gui"
-	//
-	IngressConfigMap = "map"
 	// IngressAppSuffix -> the suffix added to the path of the ingress targeting standalone and container environments.
 	IngressAppSuffix = "app"
 
@@ -40,9 +37,6 @@ const (
 
 	// IngressVNCGUIPathSuffix -> the suffix appended to the path of the ingress targeting the environment GUI websocketed vnc endpoint.
 	IngressVNCGUIPathSuffix = "vnc"
-
-	// IngressGuiPathSuffix -> the suffix appended to the path of the ingress targeting the environment gui endpoint.
-	IngressdashguiPathSuffix = "gui"
 
 	// WebsockifyRewriteEndpoint -> endpoint of the websocketed vnc server.
 	WebsockifyRewriteEndpoint = "/websockify"
@@ -72,32 +66,6 @@ func IngressSpec(host, path, certificateName, serviceName, servicePort string) n
 				},
 			},
 		}},
-	}
-}
-
-// IngressSpec forges the specification of a Kubernetes Ingress resource only for cluster
-func IngressClusterSpec(host, path, certificateName, serviceName, servicePort string) netv1.IngressSpec {
-	pathTypePrefix := netv1.PathTypePrefix
-	return netv1.IngressSpec{
-		TLS: []netv1.IngressTLS{{Hosts: []string{host}, SecretName: certificateName}},
-		Rules: []netv1.IngressRule{{
-			Host: host,
-			IngressRuleValue: netv1.IngressRuleValue{
-				HTTP: &netv1.HTTPIngressRuleValue{
-					Paths: []netv1.HTTPIngressPath{{
-						Path:     path,
-						PathType: &pathTypePrefix,
-						Backend: netv1.IngressBackend{
-							Service: &netv1.IngressServiceBackend{
-								Name: serviceName,
-								Port: netv1.ServiceBackendPort{Name: servicePort},
-							},
-						},
-					}},
-				},
-			},
-		}},
-		IngressClassName: ptr.To("nginx-ssl"),
 	}
 }
 
@@ -170,8 +138,6 @@ func IngressGUIPath(instance *clv1alpha2.Instance, environment *clv1alpha2.Envir
 		return strings.TrimRight(fmt.Sprintf("%v/%v/%v", IngressInstancePrefix, instance.UID, IngressAppSuffix), "/")
 	case clv1alpha2.ClassCloudVM, clv1alpha2.ClassVM:
 		return strings.TrimRight(fmt.Sprintf("%v/%v/%v", IngressInstancePrefix, instance.UID, IngressVNCGUIPathSuffix), "/")
-	case clv1alpha2.ClassCluster:
-		return strings.TrimRight(fmt.Sprintf("%v/%v/%v", IngressInstancePrefix, instance.UID, IngressdashguiPathSuffix), "/")
 	}
 	return ""
 }
@@ -188,8 +154,6 @@ func IngressGuiStatusURL(host string, environment *clv1alpha2.Environment, insta
 		return fmt.Sprintf("https://%v%v/%v/%v/", host, IngressInstancePrefix, instance.UID, IngressAppSuffix)
 	case clv1alpha2.ClassVM, clv1alpha2.ClassCloudVM:
 		return fmt.Sprintf("https://%v%v/%v/", host, IngressInstancePrefix, instance.UID)
-	case clv1alpha2.ClassCluster:
-		return fmt.Sprintf("https://%v:%v/", host, environment.Cluster.ClusterNet.NginxTargetPort)
 	}
 	return ""
 }
@@ -199,7 +163,7 @@ func IngressGUIName(environment *clv1alpha2.Environment) string {
 	switch environment.EnvironmentType {
 	case clv1alpha2.ClassStandalone:
 		return IngressAppSuffix
-	case clv1alpha2.ClassContainer, clv1alpha2.ClassVM, clv1alpha2.ClassCloudVM, clv1alpha2.ClassCluster:
+	case clv1alpha2.ClassContainer, clv1alpha2.ClassVM, clv1alpha2.ClassCloudVM:
 		return IngressGUINameSuffix
 	}
 	return ""
