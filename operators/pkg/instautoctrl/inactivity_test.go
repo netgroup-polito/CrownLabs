e7a31acdb906f18974b1a47dbd5ab16d51f235c8
Andrea Buonaurio <65712954+andreabuon@users.noreply.github.com>
2025-07-02 18:17:33 +0200

    feat: add initial test setup for inactivity functionality

---
+++ operators/pkg/instautoctrl/inactivity_test.go
@@ -0,0 +1,12 @@
+package instautoctrl_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/netgroup-polito/CrownLabs/operators/pkg/instautoctrl"
+)
+
+var _ = Describe("Inactivity", func() {
+
+})
