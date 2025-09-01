4ac8b9a086a8702ee4c66053038bff7dd04b8a79
Andrea Abellonio <s297484@studenti.polito.it>
2025-09-01 19:32:18 +0200

    fix: nodeSelector type

--- frontend/src/components/workspaces/Templates/TemplatesTableRow/TemplatesTableRow.tsx
+++ frontend/src/components/workspaces/Templates/TemplatesTableRow/TemplatesTableRow.tsx
@@ -42,7 +42,7 @@ export interface ITemplatesTableRowProps {
   deleteTemplateLoading: boolean;
   createInstance: (
     id: string,
-    labelSelector?: JSON,
+    labelSelector?: Record<string, string>,
   ) => Promise<
     FetchResult<
       CreateInstanceMutation,
@@ -315,8 +315,7 @@ const TemplatesTableRow: FC<ITemplatesTableRowProps> = ({ ...props }) => {
                         label: `${cleanupLabels(key)}=${value}`,
                         disabled: loadingLabels,
                         onClick: () => {
-                          console.log(JSON.parse(JSON.stringify({ [cleanupLabels(key)]: value })))
-                          createInstance(template.id, JSON.parse(JSON.stringify({ [cleanupLabels(key)]: value })))
+                          createInstance(template.id, { [key]: value })
                             .then(() => {
                               refreshClock();
                               setTimeout(setCreateDisabled, 400, false);
