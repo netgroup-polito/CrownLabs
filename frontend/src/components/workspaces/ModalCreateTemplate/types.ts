a61bf0031e2c7221b00bf32e5aadfaa49eb4fc83
FerroO2000 <immersivetech.mod@gmail.com>
2025-10-11 15:47:32 +0200

    multi-environment modal for template creation

---
+++ frontend/src/components/workspaces/ModalCreateTemplate/types.ts
@@ -0,0 +1,57 @@
+import type { EnvironmentType } from '../../../generated-types';
+
+export type Interval = {
+  max: number;
+  min: number;
+};
+
+export type Resources = {
+  cpu: Interval;
+  disk: Interval;
+  ram: Interval;
+};
+
+export type TemplateForm = {
+  name: string;
+  environments: TemplateFormEnv[];
+};
+
+export type TemplateFormEnv = {
+  name: string;
+  environmentType: EnvironmentType;
+  image: string;
+  registry: string;
+  gui: boolean;
+  cpu: number;
+  ram: number;
+  persistent: boolean;
+  disk: number;
+  sharedVolumeMounts: TemplateFormEnvShVol[];
+};
+
+export type ChildFormItem = {
+  parentFormName: number;
+  restField: {
+    fieldKey?: number | undefined;
+  };
+};
+
+export interface TemplateFormEnvShVol {
+  sharedVolume: string; // workspace/shvol
+  mountPath: string;
+  readOnly: boolean;
+}
+export type Image = {
+  name: string;
+  type: Array<EnvironmentType>;
+  registry: string;
+};
+
+export type ImageList = {
+  name: string;
+  registryName: string;
+  images: Array<{
+    name: string;
+    versions: Array<string>;
+  }>;
+};
