2e72074ab929dbdce7d89c6222c5fe3b18f3683a
Giandomenico Lacatena <s332994@studenti.polito.it>
2025-12-02 18:32:21 +0100

    fix: issue #1005 connect button in popup with correct url, quota calculation updated with all the environments in the template

--- frontend/src/components/workspaces/QuotaDisplay/useQuotaCalculation.tsx
+++ frontend/src/components/workspaces/QuotaDisplay/useQuotaCalculation.tsx
@@ -93,14 +93,19 @@ export const useQuotaCalculations = (
     const items = instances ?? [];
 
     for (const inst of items) {
-      const resources =
+      const { cpu, mem } = (
         inst?.spec?.templateCrownlabsPolitoItTemplateRef?.templateWrapper
-          ?.itPolitoCrownlabsV1alpha2Template?.spec?.environmentList?.[0]
-          ?.resources;
-      const cpu = Number(resources?.cpu ?? 0);
-      const mem = resources?.memory ?? '0Gi';
+          ?.itPolitoCrownlabsV1alpha2Template?.spec?.environmentList ?? []
+      ).reduce(
+        (acc, env) => ({
+          cpu: acc.cpu + Number(env?.resources?.cpu ?? 0),
+          mem: acc.mem + parseMemoryToGB(env?.resources?.memory ?? '0Gi'),
+        }),
+        { cpu: 0, mem: 0 }
+      );
+
       consumedQuota.cpu += cpu;
-      consumedQuota.memoryGB += parseMemoryToGB(mem);
+      consumedQuota.memoryGB += mem;
       consumedQuota.instances += 1;
     }
 
