737284a522620d0ccb43a24d1a1d63b13f4c1812
Riccardo Tornesello <riccardo.tornesello@gmail.com>
2025-12-24 21:41:21 +0000

    feat(workspace): validate minimum quota

--- operators/api/common/quota.go
+++ operators/api/common/quota.go
@@ -6,12 +6,14 @@ import "k8s.io/apimachinery/pkg/api/resource"
 // +k8s:deepcopy-gen=true
 type WorkspaceResourceQuota struct {
 	// The maximum amount of CPU required by this Workspace.
+	// +kubebuilder:validation:XValidation:rule="quantity(string(self)).compareTo(quantity('1')) >= 0",message="Minimum 1 CPU core is required"
 	CPU resource.Quantity `json:"cpu"`
 
 	// The maximum amount of RAM memory required by this Workspace.
+	// +kubebuilder:validation:XValidation:rule="quantity(string(self)).compareTo(quantity('1G')) >= 0",message="Minimum 1 GB memory is required"
 	Memory resource.Quantity `json:"memory"`
 
-	// +kubebuilder:validation:Minimum:=1
 	// The maximum number of concurrent instances required by this Workspace.
+	// +kubebuilder:validation:Minimum:=1
 	Instances int64 `json:"instances"`
 }
