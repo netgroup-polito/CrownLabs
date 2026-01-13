6891d7ae292366499b84bcff809804fec8145660
Riccardo Tornesello <riccardo.tornesello@gmail.com>
2026-01-13 15:14:39 +0000

    fix(crd): fix quantity types validation

--- operators/api/common/quota.go
+++ operators/api/common/quota.go
@@ -20,11 +20,11 @@ import "k8s.io/apimachinery/pkg/api/resource"
 // +k8s:deepcopy-gen=true
 type WorkspaceResourceQuota struct {
 	// The maximum amount of CPU required by this Workspace.
-	// +kubebuilder:validation:XValidation:rule="quantity(string(self)).compareTo(quantity('1')) >= 0",message="Minimum 1 CPU core is required"
+	// +kubebuilder:validation:XValidation:rule="quantity(self).compareTo(quantity('1')) >= 0",message="Minimum 1 CPU core is required"
 	CPU resource.Quantity `json:"cpu"`
 
 	// The maximum amount of RAM memory required by this Workspace.
-	// +kubebuilder:validation:XValidation:rule="quantity(string(self)).compareTo(quantity('1G')) >= 0",message="Minimum 1 GB memory is required"
+	// +kubebuilder:validation:XValidation:rule="quantity(self).compareTo(quantity('1Gi')) >= 0",message="Minimum 1 GB of RAM is required"
 	Memory resource.Quantity `json:"memory"`
 
 	// The maximum number of concurrent instances required by this Workspace.
