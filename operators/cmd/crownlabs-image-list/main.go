557e84f2ab7eb5e5c644a0154f6def20bae56409
Riccardo Marco Miracapillo s324163 <s324163@studenti.polito.it>
2026-04-07 13:06:43 +0200

    delete update interval

--- operators/cmd/crownlabs-image-list/main.go
+++ operators/cmd/crownlabs-image-list/main.go
@@ -15,7 +15,6 @@ func main() {
 		advertisedRegistryName string
 		imageListName          string
 		registryURL            string
-		updateIntervalSec      int
 		registryUsername       string
 		registryPassword       string
 	)
@@ -23,19 +22,17 @@ func main() {
 	flag.StringVar(&advertisedRegistryName, "advertised-registry-name", "", "Hostname of the Docker registry as advertised to consumers")
 	flag.StringVar(&imageListName, "image-list-name", "", "Base name/prefix for ImageList objects")
 	flag.StringVar(&registryURL, "registry-url", "", "Docker registry base URL (e.g. https://registry.example.com)")
-	flag.IntVar(&updateIntervalSec, "update-interval", 60, "Interval in seconds between updates")
 	flag.StringVar(&registryUsername, "registry-username", "", "Main Resitry User Name")
 	flag.StringVar(&registryPassword, "registry-password", "", "Main Registry Password")
 	klog.InitFlags(nil)
 	flag.Parse()
 
 	log := textlogger.NewLogger(textlogger.NewConfig()).WithName("imageList")
-	if advertisedRegistryName == "" || imageListName == "" || registryURL == "" || registryUsername == "" || registryPassword == "" || updateIntervalSec <= 0 {
+	if advertisedRegistryName == "" || imageListName == "" || registryURL == "" || registryUsername == "" || registryPassword == "" {
 		log.Error(nil, `Usage:
 			--advertised-registry-name <registry-name>
 			--image-list-name <list-name>
 			--registry-url <url>
-			--update-interval <seconds>
 			`)
 		os.Exit(1)
 	}
@@ -59,14 +56,6 @@ func main() {
 	}
 
 	up := imageList.NewImageListUpdater(imageListRequestors, imageListName, advertisedRegistryName, log.WithName("crownlabs-imagelists-updater"))
-
-	// without cronjob
-	// for {
-	// 	up.RunUpdateProcess()
-	// 	// t := time.NewTicker(time.Duration(updateIntervalSec) * time.Second)
-	// 	time.Sleep(time.Duration(updateIntervalSec) * time.Second)
-	// }
-	///
 
 	// with the cronjob
 	up.RunUpdateProcess()
