1f89fe217808e2d73e64448fbe3c42102f9b8dea
Riccardo Tornesello <riccardo.tornesello@linksfoundation.com>
2026-04-29 18:29:57 +0200

    feat: show tenant's registration and last login date

--- frontend/src/components/tenants/TenantInfo/TenantInfo.tsx
+++ frontend/src/components/tenants/TenantInfo/TenantInfo.tsx
@@ -8,7 +8,6 @@ export interface ITenantInfoProps {
 const TenantInfo: FC<ITenantInfoProps> = ({ tenant }) => {
   return (
     <>
-      <h3>Personal</h3>
       <p>
         First name: <strong>{tenant.tenant?.spec?.firstName}</strong>
       </p>
@@ -17,6 +16,27 @@ const TenantInfo: FC<ITenantInfoProps> = ({ tenant }) => {
       </p>
       <p>
         Email: <strong>{tenant.tenant?.spec?.email}</strong>
+      </p>
+
+      <hr className="my-4" />
+
+      <p>
+        Registration date:
+        <strong>
+          {tenant.tenant?.metadata?.creationTimestamp
+            ? new Date(
+                tenant.tenant.metadata.creationTimestamp,
+              ).toLocaleDateString()
+            : 'N/A'}
+        </strong>
+      </p>
+      <p>
+        Last login:{' '}
+        <strong>
+          {tenant.tenant?.spec?.lastLogin
+            ? new Date(tenant.tenant.spec.lastLogin).toLocaleDateString()
+            : 'N/A'}
+        </strong>
       </p>
     </>
   );
