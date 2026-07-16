972319252b46a7bd5962ef092ca0845e18153685
Andrea Abellonio <s343884@studenti.polito.it>
2026-07-16 17:51:13 +0200

    fix lint format

--- operators/pkg/instctrl/virtualmachines.go
+++ operators/pkg/instctrl/virtualmachines.go
@@ -82,7 +82,6 @@ func (r *InstanceReconciler) enforceVirtualMachine(ctx context.Context) error {
 			// Forge the DataVolume specifications only at creation time, as changing them later may be either rejected by the webhook or cause data loss.
 			dv.Spec = forgedDV
 		}
-		
 		// Remove the owner
 		dv.OwnerReferences = nil
 
