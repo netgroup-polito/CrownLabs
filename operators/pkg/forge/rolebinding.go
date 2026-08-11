af172e404be2968b2098ad55df6bb236ed957057
Andrea Abellonio <s343884@studenti.polito.it>
2026-08-11 20:30:50 +0200

    refactor: aggregate workspace manager permissions into a single ClusterRole and migrate existing RoleBindings

--- operators/pkg/forge/rolebinding.go
+++ operators/pkg/forge/rolebinding.go
@@ -31,6 +31,9 @@ const (
 	// ManageTemplatesRoleName -> the name of the ClusterRole for managing templates in workspaces.
 	ManageTemplatesRoleName = "crownlabs-manage-templates"
 
+	// WorkspaceManagerRoleName -> the name of the ClusterRole for managers in workspaces.
+	WorkspaceManagerRoleName = "crownlabs-workspace-manager"
+
 	// ManageSharedVolumesRoleName -> the name of the ClusterRole for managing shared volumes in workspaces.
 	ManageSharedVolumesRoleName = "crownlabs-manage-sharedvolumes"
 )
@@ -60,8 +63,8 @@ func ConfigureWorkspaceUserViewTemplatesBinding(ws *clv1alpha1.Workspace, rb *rb
 	}
 }
 
-// ConfigureWorkspaceManagerManageTemplatesBinding configures a RoleBinding for a workspace manager to manage templates.
-func ConfigureWorkspaceManagerManageTemplatesBinding(ws *clv1alpha1.Workspace, rb *rbacv1.RoleBinding, labels map[string]string) {
+// ConfigureWorkspaceManagerAggregatedBinding configures a RoleBinding for a workspace manager to access all manager privileges.
+func ConfigureWorkspaceManagerAggregatedBinding(ws *clv1alpha1.Workspace, rb *rbacv1.RoleBinding, labels map[string]string) {
 	// Set labels
 	if rb.Labels == nil {
 		rb.Labels = make(map[string]string)
@@ -73,32 +76,7 @@ func ConfigureWorkspaceManagerManageTemplatesBinding(ws *clv1alpha1.Workspace, r
 	// Configure RoleRef
 	rb.RoleRef = rbacv1.RoleRef{
 		Kind:     "ClusterRole",
-		Name:     ManageTemplatesRoleName,
-		APIGroup: rbacv1.GroupName,
-	}
-
-	// Configure Subjects
-	rb.Subjects = []rbacv1.Subject{
-		{
-			Kind:     rbacv1.GroupKind,
-			Name:     fmt.Sprintf("kubernetes:%s", WorkspaceRoleName(ws.Name, clv1alpha2.Manager)),
-			APIGroup: rbacv1.GroupName,
-		},
-	}
-}
-
-// ConfigureWorkspaceManagerManageSharedVolumesBinding configures a RoleBinding for a workspace manager to manage shared volumes.
-func ConfigureWorkspaceManagerManageSharedVolumesBinding(ws *clv1alpha1.Workspace, rb *rbacv1.RoleBinding, labels map[string]string) {
-	// Set labels
-	if rb.Labels == nil {
-		rb.Labels = make(map[string]string)
-	}
-	maps.Copy(rb.Labels, labels)
-
-	// Configure RoleRef
-	rb.RoleRef = rbacv1.RoleRef{
-		Kind:     "ClusterRole",
-		Name:     ManageSharedVolumesRoleName,
+		Name:     WorkspaceManagerRoleName,
 		APIGroup: rbacv1.GroupName,
 	}
 
